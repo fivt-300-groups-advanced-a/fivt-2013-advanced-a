7d5ceb4ab6c0c5a647bf42d1989707e7702459b4
Feodor Alexeev <feodor.alexeev@gmail.com>
2013-09-09 23:14:03 +0400

    -a

--- Alexeev/sort.cpp
+++ Alexeev/sort.cpp
@@ -2,33 +2,32 @@
 #include <vector>
 #include <string>
 #include <algorithm>
+#include <sstream>
 
 #include <cstdlib>
-#include <cassert>
 
 using namespace std;
 
-class Token
+void myAssert(bool condition, const char *msg)
+{
+	if (!condition)
+	{
+		cerr << "ASSERT " << msg;
+		abort();
+	}
+}
+#define myAssert(x) myAssert(x, "at line " __LINE__ " on condition "#x)
+
+class ComparableToken
 {
 	string _raw;
 	bool _isNumber;
-    int _number;
+	int _number;
 
 public:
-	explicit Token(const string &s): _raw(s)
+	explicit ComparableToken(const string &s): _raw(s)
 	{
-		assert(!s.empty());
-		_isNumber = true;
-		for (size_t i = 0; i < s.size(); ++i)
-		{
-			if (isdigit(s[i]))
-				assert(_isNumber);
-			else
-			{
-				assert(!i || !_isNumber);
-				_isNumber = false;
-			}
-		}
+		_isNumber = isdigit(s[0]);
 		if (_isNumber)
 			_number = atoi(s.c_str());
 	}
@@ -37,7 +36,7 @@ public:
 	bool isNumber() const { return _isNumber; }
 	int number() const { return _number; }
 	
-	bool operator < (const Token &t) const
+	bool operator < (const ComparableToken &t) const
 	{
 		if (_isNumber && t._isNumber && _number != t._number)
 			return _number < t._number;
@@ -45,9 +44,9 @@ public:
 	}
 };
 
-vector<Token> tokenize(const string &s)
+vector<string> tokenize(const string &s)
 {
-	vector<Token> result;
+	vector<string> result;
 	if (s.empty())
 		return result;
 
@@ -60,38 +59,59 @@ vector<Token> tokenize(const string &s)
 			cur += c;
 		else
 		{
-			result.push_back(Token(cur));
+			result.push_back(cur);
 			isNumber = !isNumber;
 			cur = string(1, c);
 		}
 	}
-	if (!cur.empty())
-		result.push_back(Token(cur));
+	result.push_back(cur);
 	return result;
 }
 
+vector<ComparableToken> buildVectorOfComparableTokens(const vector<string> &v)
+{
+	vector<ComparableToken> result;
+	result.reserve(v.size());
+	for (size_t i = 0; i < v.size(); ++i)
+		result.push_back(ComparableToken(v[i]));
+	return result;
+}
+
+class Solver
+{
+public:
+	void solve(istream &in, ostream &out)
+	{
+		vector< vector<ComparableToken> > tokenLines;
+		string line;
+		while (in >> line)
+		{
+			cin >> line;
+			tokenLines.push_back(buildVectorOfComparableTokens(tokenize(line)));
+		}
+		
+		sort(tokenLines.begin(), tokenLines.end());
+	
+		for (auto it = tokenLines.begin(); it != tokenLines.end(); ++it)
+		{
+			for (auto jt = it->begin(); jt != it->end(); ++jt)
+				out << jt->rawString();
+			out << endl;
+		}
+	}
+	string solve(const string &text)
+	{
+		istringstream in(text);
+		ostringstream out;
+		solve(in, out);
+		return out.toString();
+	}
+};
+
 int main()
 {
-	int n;
-	cin >> n;
-
-	vector< vector<Token> > tokenLines;
-	tokenLines.reserve(n);
-	for (int i = 0; i < n; ++i)
-	{
-		string line;
-		cin >> line;
-		tokenLines.push_back(tokenize(line));
-	}
-	
-	sort(tokenLines.begin(), tokenLines.end());
-
-	for (int i = 0; i < n; ++i)
-	{
-		for (vector<Token>::iterator it = tokenLines[i].begin(), end = tokenLines[i].end(); it != end; ++it)
-			cout << it->rawString();
-		cout << endl;
-	}		
+	Solver solver;
+	solver.solve(cin, cout);
 
 	return 0;
 }
