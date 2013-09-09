7cd365213823f48173618fd14568413829240ffa
Mark Ryabov <m.ryadom@gmail.com>
2013-09-09 12:50:15 +0400

    Добавлен unit test

--- Ryabov/sort.cpp
+++ Ryabov/sort.cpp
@@ -30,6 +30,11 @@ public:
 		str = s;
 	}
 
+	bool isStr()
+	{
+		return isString;
+	}
+
 	string getstr()
 	{
 		return this->str;
@@ -47,24 +52,60 @@ public:
 	}
 };
 
-vector<elem> build(string s)
+vector<elem> build(vector<string> v)
 {
-	vector<elem> v;
+	vector<elem> r;
+	for (int i = 0; i < v.size(); ++i)
+		r.push_back(elem(v[i]));
+	return r;
+}
+
+vector<string> split(string s)
+{
+	vector<string> v;
 	string e;
 	for (int i = 0; i < s.length(); ++i)
 	{
 		if (i == 0 || (isdigit(e[0]) != isdigit(s[i])))
 		{
 			if (i != 0)
-				v.push_back(elem(e));
+				v.push_back(e);
 			e = s[i];
 		}
 		else
 			e += s[i];
 	}
-	v.push_back(elem(e));
+	if (e.size() > 0)
+		v.push_back(e);
 	return v;
 }
+
+#ifdef TEST_MODE
+int main()
+{
+	vector<string> v;
+	v.push_back("1");
+	v.push_back("abs.,");
+	v.push_back("234");
+	v.push_back("jj");
+	v.push_back("2");
+	v.push_back("gZ");
+	v.push_back("1");
+	cout << "TEST1 -> " << (split("1abs.,234jj2gZ1") == v ? "OK" : "FAIL") << endl;
+	v.clear();
+	cout << "TEST2 -> " << (split("") == v ? "OK" : "FAIL") << endl;
+	v.push_back("123");
+	cout << "TEST3 -> " << (split("123") == v ? "OK" : "FAIL") << endl;
+	cout << "TEST4 -> " << (elem("123").isStr() == false ? "OK" : "FAIL") << endl;
+	cout << "TEST5 -> " << (elem("abc").isStr() == true ? "OK" : "FAIL") << endl;
+	cout << "TEST6 -> " << (elem("123").getstr() == "123" ? "OK" : "FAIL") << endl;
+	cout << "TEST7 -> " << ((elem("abc") < elem("123")) == true ? "OK" : "FAIL") << endl;
+	cout << "TEST8 -> " << ((elem("011") < elem("123")) == true ? "OK" : "FAIL") << endl;
+	cout << "TEST9 -> " << ((elem("1") < elem("123")) == true ? "OK" : "FAIL") << endl;
+	cout << "TEST10 -> " << ((elem("abc") < elem("abd")) == true ? "OK" : "FAIL") << endl;
+	return 0;
+}
+#else
 
 int main()
 {
@@ -76,7 +117,7 @@ int main()
 	for (int i = 0; i < n; ++i)
 	{
 		getline(cin, s);
-		v.push_back(build(s));
+		v.push_back(build(split(s)));
 	}
 	sort(v.begin(), v.end());
 	for (int i = 0; i < n; ++i)
@@ -86,3 +127,4 @@ int main()
 		cout << endl;
 	}
 }
+#endif
