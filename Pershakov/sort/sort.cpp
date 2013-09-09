d77627d0e1857b4137f7b1516e651c625da22555
pershik <k.pershik@gmail.com>
2013-09-09 21:22:44 +0400

    Changed sort.cpp

--- Pershakov/sort/sort.cpp
+++ Pershakov/sort/sort.cpp
@@ -24,159 +24,200 @@ bool cmp_as_number(const string &a, const string &b){
     return a.length() > b.length();
 }
 
-struct token {
-    string s;
+class Token {
 
-    token(const string &x){
-        s = x;
+    string token;
+
+public:
+
+    explicit Token(const string &val){
+        token = val;
     }
 
-    bool operator < (const token &t) const {
-        if (get_type(s[0]) != get_type(t.s[0]))
-            return (get_type(s[0]) == LETTER? true : false);
-        if (get_type(s[0]) == DIGIT)
-            return cmp_as_number(s, t.s);
-        if (get_type(s[0]) == LETTER)
-            return s < t.s;
+    bool operator < (const Token &t) const {
+        if (get_type(token[0]) != get_type(t.token[0]))
+            return (get_type(token[0]) == LETTER? true : false);
+        if (get_type(token[0]) == DIGIT)
+            return cmp_as_number(token, t.token);
+        if (get_type(token[0]) == LETTER)
+            return token < t.token;
         assert(false);
+    }
+
+    void print() const{
+        cout << token;
+    }
+
+    string get_token(){
+        return token;
     }
 
 };
 
-void print(vector<token> &a){
+void print(const vector<Token> &a){
     for (int i = 0; i < (int)a.size(); i++)
-        cout << a[i].s;
+        a[i].print();
     cout << endl;
 }
 
-vector<token> tokenize(string &s){
+vector<Token> tokenize(string &s){
     string buf = "";
-    vector<token> r;
+    vector<Token> r;
     int cur_type = UNKNOWN;
     for (int j = 0; j < (int)s.length(); j++){
         if (get_type(s[j]) == cur_type)
             buf += s[j];
         else {
             if (cur_type != UNKNOWN)
-                r.push_back(token(buf));
+                r.push_back(Token(buf));
             buf = "";
             buf += s[j];
             cur_type = get_type(s[j]);
         }
     }
-    r.push_back(token(buf));
+    r.push_back(Token(buf));
     return r;
 }
 
 void test_tokenizer(){
-    cout << "Testing tokenize\n";
-    vector<token> res;
+    cerr << "Testing tokenize\n";
+    vector<Token> res;
     string s;
 
     s = "12dfre56gf";
     res = tokenize(s);
     if (!(res.size() == 4 && 
-            res[0].s == "12" && res[1].s == "dfre" && res[2].s == "56" &&
-            res[3].s == "gf")){
-        cout << "Bad tokenize #1" << endl;
+            res[0].get_token() == "12" && res[1].get_token() == "dfre" 
+            && res[2].get_token() == "56" && res[3].get_token() == "gf")){
+        cerr << "Bad tokenize #1" << endl;
         exit(0);
     }
 
     s = "fd45ggg22g";
     res = tokenize(s);
     if (!(res.size() == 5 && 
-            res[0].s == "fd" && res[1].s == "45" && res[2].s == "ggg" &&
-            res[3].s == "22") && res[4].s == "g"){
-        cout << "Bad tokenize #2" << endl;
+            res[0].get_token() == "fd" && res[1].get_token() == "45" 
+            && res[2].get_token() == "ggg" && res[3].get_token() == "22" 
+            && res[4].get_token() == "g")){
+        cerr << "Bad tokenize #2" << endl;
         exit(0);
     }
 
     s = "567";
     res = tokenize(s);
-    if (!(res.size() == 1 && res[0].s == "567")){
-        cout << "Bad tokenize #4" << endl;
+    if (!(res.size() == 1 && res[0].get_token() == "567")){
+        cerr << "Bad tokenize #4" << endl;
         exit(0);
     }
 
     s = "dsfa";
     res = tokenize(s);
-    if (!(res.size() == 1 && res[0].s == "dsfa")){
-        cout << "Bad tokenize #5" << endl;
+    if (!(res.size() == 1 && res[0].get_token() == "dsfa")){
+        cerr << "Bad tokenize #5" << endl;
         exit(0);
     }
 
-    cout << "Testing tokenize: OK\n";    
+    cerr << "Testing tokenize: OK\n";    
 }
 
 void test_cmp_as_number(){
-    cout << "Testing cmp_as_number\n";
+    cerr << "Testing cmp_as_number\n";
     
     if (cmp_as_number("3", "3")){
-        cout << "Bad cmp_as_number #1\n";
+        cerr << "Bad cmp_as_number #1\n";
         exit(0);
     }
 
     if (cmp_as_number("4", "3")){
-        cout << "Bad cmp_as_number #2\n";
+        cerr << "Bad cmp_as_number #2\n";
         exit(0);
     }
 
     if (!cmp_as_number("3", "4")){
-        cout << "Bad cmp_as_number #3\n";
+        cerr << "Bad cmp_as_number #3\n";
         exit(0);
     }
 
     if (!cmp_as_number("3", "33")){
-        cout << "Bad cmp_as_number #4\n";
+        cerr << "Bad cmp_as_number #4\n";
         exit(0);
     }
 
     if (cmp_as_number("33", "3")){
-        cout << "Bad cmp_as_number #5\n";
+        cerr << "Bad cmp_as_number #5\n";
         exit(0);
     }
 
     if (cmp_as_number("3", "03")){
-        cout << "Bad cmp_as_number #6\n";
+        cerr << "Bad cmp_as_number #6\n";
         exit(0);
     }
     
     if (!cmp_as_number("03", "3")){
-        cout << "Bad cmp_as_number #7\n";
+        cerr << "Bad cmp_as_number #7\n";
         exit(0);
     }
 
-    cout << "Testing cmp_as_number: OK\n";
+    cerr << "Testing cmp_as_number: OK\n";
 }
 
 void test_get_type(){
-    cout << "Testing get_type\n";
+    cerr << "Testing get_type\n";
 
     if (get_type('3') != DIGIT){
-        cout << "Bad get_type #1\n";
+        cerr << "Bad get_type #1\n";
         exit(0);
     }
 
     if (get_type('a') != LETTER){
-        cout << "Bad get_type #2\n";
+        cerr << "Bad get_type #2\n";
         exit(0);
     }
 
-    cout << "Testing get_type: OK\n";
+    cerr << "Testing get_type: OK\n";
 }
 
 void test_cmp_token(){
-    cout << "Testing cmp_token\n";
+    cerr << "Testing cmp_token\n";
+    
+    if (Token("ab") < Token("aa")){
+        cerr << "Bad cmp_token #1\n";
+        exit(0);
+    }
+
+    if (Token("3") < Token("a")){
+        cerr << "Bad cmp_token #2\n";
+        exit(0);
+    }
+
+    if (Token("334") < Token("33")){
+        cerr << "Bad cmp_token #3\n";
+        exit(0);
+    }
+
+    if (Token("abcd") < Token("abc")){
+        cerr << "Bad cmp_token 4\n";
+        exit(0);
+    }
+
+    if (Token("001") < Token("0001")){
+        cerr << "Bad cmp_token #5\n";
+        exit(0);
+    }
+
+    cerr << "Testing cmp_token: OK\n";
 }
 
 void unit_test(){
+    cerr << "Start testing\n";
     test_get_type();
     test_tokenizer();
     test_cmp_as_number();
     test_cmp_token();
+    cerr << "Testing: OK\n";
 }
 
-vector<vector<token> > a;
+vector<vector<Token> > a;
 
 int main(){
     unit_test();
