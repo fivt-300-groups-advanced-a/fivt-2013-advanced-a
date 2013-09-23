93b0ce05ea8e73e730356355a7b8937a56d3f776
pershik <k.pershik@gmail.com>
2013-09-23 12:35:10 +0400

    variables are renamed

--- Pershakov/external_sort/input/text_file_reader.h
+++ Pershakov/external_sort/input/text_file_reader.h
@@ -5,18 +5,18 @@
 #include <string>
 #include <fstream>
 
-template <class T> class Text_file_reader {
+template <class T> class TextFileReader {
     public:
-        explicit Text_file_reader(std::ifstream &new_stream){
+        explicit TextFileReader(std::ifstream &new_stream){
             this->in = &new_stream;
         }
 
-        explicit Text_file_reader(std::string file_name){
+        explicit TextFileReader(std::string file_name){
             this->in = 
                 new std::ifstream(file_name.c_str(), std::ifstream::in); 
         }
 
-        explicit Text_file_reader(char *file_name){
+        explicit TextFileReader(char *file_name){
             this->in = 
                 new std::ifstream(file_name, std::ifstream::in);
         }
