8c871c7fbdeb634b20d87ac8b3c30b84ded29889
Daria Kolodzey <kolodaria@google.com>
2013-09-30 03:33:43 +0400

    improved makefile for tests

--- Kolodzey/bigsort/tests/bubblesort_unittest.cpp
+++ Kolodzey/bigsort/tests/bubblesort_unittest.cpp
@@ -4,9 +4,9 @@
 
 TEST(BubblesortTest, Trivial)
 {
-	int a[5] = {0, 1, 3, 5, 4};
+	int a[5] = {2, 1, 0, 5, 4};
 	bubblesort(a, 5);
-	int b[5] = {0, 1, 3, 5, 4};
+	int b[5] = {2, 1, 0, 5, 4};
 	std::sort(b, b + 5);
 	for (int i = 0; i < 5; ++i)
 		EXPECT_EQ(a[i], b[i]);
