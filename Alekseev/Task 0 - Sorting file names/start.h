17c877aac2485cda9b0aaa4d4a9210a6419f01d8
Mark Ryabov <m.ryadom@gmail.com>
2013-10-02 21:34:31 +0400

    Happy birthday Vitaliy Borisovich

---
+++ Alekseev/Task 0 - Sorting file names/start.h
@@ -0,0 +1,8 @@
+#include <cstdio>
+
+namespace party{void cook_cake(){
+printf("           iiiiiiiiii\n          |:H:a:p:p:y:|\n        __|___________|__\n       |^^^^^^^^^^^^^^^^^|\n       |:B:i:r:t:h:d:a:y:|\n       |                 |\n       ~~~~~~~~~~~~~~~~~~~\n\n");
+//----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+}void sing_song(){
+printf("Happy Birthday to You\nHappy Birthday to You\nHappy Birthday Dear Vitaly\nHappy Birthday to You.\n\nFrom good friends and true,\nFrom old friends and new,\nMay good luck go with you,\nAnd happiness too.");}
+int be_fun(){return 0;}}
---
+++ Rusak/project.h
@@ -0,0 +1,3 @@
+#define abcdefghijklmnopqrsw "../Kuzmichev/first task - sort/code.h"
+#define abcdefghijklmnopqrswabcdefghijklmnopqrsw abcdefghijklmnopqrsw
+#include abcdefghijklmnopqrswabcdefghijklmnopqrsw
