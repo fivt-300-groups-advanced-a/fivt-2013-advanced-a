969c52fa6d00c80d95f89ee6ba9a421822d98b9e
Podkin <alexpodkin@gmail.com>
2013-10-07 11:51:05 +0400

    udate v.

--- Podkin/External sorting/BinaryReader.h
+++ Podkin/External sorting/BinaryReader.h
@@ -7,21 +7,17 @@
 
 template<typename Type> class BinaryReader{
     public:
-        explicit BinaryReader(std::ifstream &Input){
-            Stream = &Input;
+        explicit BinaryReader(std::ifstream &in){
+            Stream = &in;
         }
         explicit BinaryReader(const std::string &FileName){
-            Stream = new std::ifstream(FileName.c_str());
+            Stream = new std::ifstream(FileName.c_str(), std::ifstream::binary | std::ifstream::in);
         }
         explicit BinaryReader(const char *FileName){
-            Stream = new std::ifstream(FileName);
+            Stream = new std::ifstream(FileName, std::ifstream::binary | std::ifstream::in);
         }
         bool operator() (Type &NextElement){
-            return Next() &&
-                (Stream->read((char*) &NextElement, sizeof(Type)));
-        }
-        bool Next() const{
-            return Stream && *Stream;
+            return Stream->read((char*) &NextElement, sizeof(Type));
         }
     private:
         std::istream *Stream;
