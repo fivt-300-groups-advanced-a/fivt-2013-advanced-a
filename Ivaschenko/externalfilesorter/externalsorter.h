d9ceb67b3d5a855da84932562c0cce1574e06f41
Skird <grafoman96@gmail.com>
2013-10-12 00:22:55 +0400

    Changes: 	- Added two template arguments to ExternalSorter::sort: TemporaryReader & TemporaryWriter. User can specify the way to write his data to temporary file or use default method 	- Moved creation of binary heap to ExternalSorter::mergeFiles() method 	- BinaryFileReader/Writer are now compatible with TemporaryReader/Writer interface (constructor(unsigned int streamID))

--- Ivaschenko/externalfilesorter/externalsorter.h
+++ Ivaschenko/externalfilesorter/externalsorter.h
@@ -25,59 +25,61 @@ template<typename DataType> class ExternalSorter
 		 * Sorts each piece and writes them into files in binary format
 		 * Then performs merging of this files using mergesort algorithm with binary heap
 		 * Outputs result using Writer
-		 * Default values are:
-		 *		- InputStreamReader<DataType> for Reader
-		 *		- OutputStramWriter<DataType> for Writer
-		 *		- std::sort for Sorter
-		 *		- std::less for Comparator
 		 * Uses not more than (avaialbeMemory) bytes for storing data at any moment
 		 * Uses own realisation of binary heap because std::priority_queue is slow and inconvinient
 		 * Returns true if succeeded and false if error occured or data set is empty
 		 * Important: default sorting is unstable
+		 *
+		 * Template arguments:
+		 *		Reader and Writer - functors that somehow reads input and writes final output
+		 *		Sorter - functor defining sorting algorithm (accepts two iterators and comparator)
+		 *		Comparator - functor defining operator < for DataType = std::less<DataType> by default
+		 *
+		 *		TemporaryReader and Temporary writers are functors for optimised (not for human reading)
+		 *		temporary input-output. Both must have constructor with unsigned int parameter (stream ID).
+		 *		Default values are BinaryFileReader<DataType> and BinaryFileWriter<DataType>
+		 *
+		 *		(!) Important: TemporaryReader and Writer must be organised in a such way that
+		 *		data wrote to i-th stream with TemporaryWriter(i) could be read by TemporaryReader(i)
+		 *		stream ID is always unsigned integer
+		 *		Readers and writers will be creater by operator new and destroyed by delete
 		 */
-		template<typename Reader, typename Writer, typename Sorter, typename Comparator>
+		template<typename Reader, typename Writer,
+				 typename Sorter, typename Comparator,
+				 typename TemporaryReader = BinaryFileReader<DataType>,
+				 typename TemporaryWriter = BinaryFileWriter<DataType> >
 		bool sort(  std::size_t availableMemory,
-					Reader &reader,
-					Writer &writer,
-					Sorter sorter,
-					Comparator comparator = std::less<DataType>())
+					Reader &reader, Writer &writer,
+					Sorter sorter, Comparator comparator)
 		{
-			cleanUp();
 			int bufferSize = availableMemory / sizeof(DataType);
 			if (!bufferSize) return false;
-			if (!readData(bufferSize, reader, sorter, comparator))
-			{
-				cleanUp();
+			if (!readData<Reader, TemporaryWriter, Sorter, Comparator>(bufferSize, reader, sorter, comparator))
 				return false;
-			}
-			BinaryHeap<DataType> heap;
-			bool success = mergeFiles(writer, comparator, heap);
-			cleanUp();
+
+			bool success = mergeFiles<Writer, TemporaryReader, Comparator, BinaryHeap<DataType> >(writer, comparator);
 			return success;
 		}
 
 		/*
 		 * Stable variant of ExternalFileSorter::sort function
+		 * (see ExtenalFileSorter::sort function for details)
 		 * Important: requires stable Sorter
 		 */
-		template<typename Reader, typename Writer, typename Sorter, typename Comparator>
+		template<typename Reader, typename Writer,
+				 typename Sorter, typename Comparator,
+				 typename TemporaryReader = BinaryFileReader<DataType>,
+				 typename TemporaryWriter = BinaryFileWriter<DataType> >
 		bool stableSort(std::size_t availableMemory,
-				  Reader &reader,
-				  Writer &writer,
-				  Sorter sorter,
-				  Comparator comparator = std::less<DataType>())
+				  Reader &reader, Writer &writer,
+				  Sorter sorter, Comparator comparator)
 		{
-			cleanUp();
 			int bufferSize = availableMemory / sizeof(DataType);
 			if (!bufferSize) return false;
-			if (!readData(bufferSize, reader, sorter, comparator))
-			{
-				cleanUp();
+			if (!readData<Reader, TemporaryWriter, Sorter, Comparator>(bufferSize, reader, sorter, comparator))
 				return false;
-			}
-			StableBinaryHeap<DataType> heap;
-			bool success = mergeFiles(writer, comparator, heap);
-			cleanUp();
+
+			bool success = mergeFiles<Writer, TemporaryReader, Comparator, StableBinaryHeap<DataType> >(writer, comparator);
 			return success;
 		}
 
@@ -85,37 +87,16 @@ template<typename DataType> class ExternalSorter
 		std::size_t tempFiles, dataSize;
 
 		/*
-		 * Returns temporary file name #number.
-		 * TODO: maybe it worth to insert some obfuscation to resolve file name conflicts
-		 */
-		std::string getFileName(int number)
-		{
-			char name[100]; // FIXME: fixed size
-			sprintf(name, "%d", number);
-			return ".part" + std::string(name) + ".tmp";
-		}
-
-		/*
-		 * Cleans up all created files
-		 * Returns true if succeeded
-		 */
-		bool cleanUp()
-		{
-			for (std::size_t i = 0; i < tempFiles; ++i)
-				if (remove(getFileName(i).c_str())) return false;
-			tempFiles = 0;
-			return true;
-		}
-
-		/*
 		 * Writes an array of data to file in binary format
 		 * Returns true if succeeded
 		 */
-		bool writeFile(const std::string &name, DataType *start, int items)
+		template<typename TemporaryWriter>
+		bool writeFile(unsigned int id, DataType *start, int items)
 		{
-			BinaryFileWriter<DataType> writer(name);
+			TemporaryWriter *writer = new TemporaryWriter(id);
 			for (; items; --items, ++start)
-				if (!writer(*start)) return false;
+				if (!writer->operator()(*start)) return false;
+			delete writer;
 			return true;
 		}
 
@@ -123,7 +104,7 @@ template<typename DataType> class ExternalSorter
 		 * Reads data from reader to buffer, sorts it and outputs to temporary files
 		 * Returns number of created files (less or equal 0 if error)
 		 */
-		template<typename Reader, typename Sorter, typename Comparator>
+		template<typename Reader, typename TemporaryWriter, typename Sorter, typename Comparator>
 		int readData(std::size_t bufferSize, Reader &reader, Sorter &sorter, Comparator &comparator)
 		{
 			DataType *buffer = new DataType[bufferSize];
@@ -137,7 +118,7 @@ template<typename DataType> class ExternalSorter
 				if (currentSize == bufferSize)
 				{
 					sorter(buffer, buffer + currentSize, comparator);
-					if (!writeFile(getFileName(tempFiles), buffer, currentSize)) return 0;
+					if (!writeFile<TemporaryWriter>(tempFiles, buffer, currentSize)) return 0;
 					currentSize = 0;
 					++tempFiles;
 				}
@@ -145,7 +126,7 @@ template<typename DataType> class ExternalSorter
 			if (currentSize != 0)
 			{
 				sorter(buffer, buffer + currentSize, comparator);
-				if (!writeFile(getFileName(tempFiles), buffer, currentSize)) return 0;
+				if (!writeFile<TemporaryWriter>(tempFiles, buffer, currentSize)) return 0;
 				currentSize = 0;
 				++tempFiles;
 			}
@@ -159,16 +140,18 @@ template<typename DataType> class ExternalSorter
 		 * Uses mergesort algorithm with binary heap (HeapClass)
 		 * Returns true if no error occured
 		 */
-		template<typename Writer, typename Comparator, typename HeapClass>
-		bool mergeFiles(Writer &writer, Comparator &comparator, HeapClass &heap)
+		template<typename Writer, typename TemporaryReader, typename Comparator, typename HeapClass>
+		bool mergeFiles(Writer &writer, Comparator &comparator)
 		{
 			assert(tempFiles != 0);
 			std::vector< BinaryFileReader<DataType>* > streams(tempFiles);
+
+			HeapClass heap;
 			heap.data = new std::pair<DataType, int>[tempFiles];
 
 			for (std::size_t i = 0; i < tempFiles; i++)
 			{
-				streams[i] = new BinaryFileReader<DataType>(getFileName(i));
+				streams[i] = new TemporaryReader(i);
 				streams[i]->operator() (heap.data[i].first);
 				heap.data[i].second = i;
 			}
