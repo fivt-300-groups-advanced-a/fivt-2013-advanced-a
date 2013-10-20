468fe108ec356f4a55b112ad5f7183120d6a9af0
Zhuravlyov <aa_Zhur@mail.ru>
2013-10-21 03:37:34 +0400

    Добавлены тесты

--- Zhuravlyov/External_Sort/main.cpp
+++ Zhuravlyov/External_Sort/main.cpp
@@ -2,41 +2,12 @@
 #include "Readers.h"
 #include "Sorters.h"
 #include "mainSorter.h"
-void test_genere()
-{
-	TxtFileWriter output("input.txt");
-	for (int i=0;i<1000000;i++)
-		output(static_cast<double>(rand()) / RAND_MAX);
-	output.close();
-}
+#include "gtest/gtest.h"
+#include "Tests.h"
 
-void test_check()
+int main(int argc,char** argv)
 {
-	TxtFileReader input("output.txt");
-	int x,y;
-	input(x);
-	bool f=1;
-	while (input(y)) {
-		if (y<x) f=0; 
-		x=y;
-	}
-	if (f) std::cout << "OK";
-	else std::cout << "BAD";
-	input.close();
-}
-int main()
-{
-	test_genere();
-	
-	TxtFileReader input("input.txt");
-	TxtFileWriter output("output.txt");
-	StandartSorter sort;
-	MainSorter<double> globalSorter;
-	std::less<double> cmp;
-	globalSorter(1<<16,input,output,sort,cmp);
-	input.close();
-	output.close();
-
-	test_check();
-	getchar();
+	::testing::InitGoogleTest(&argc,argv);
+	RUN_ALL_TESTS();
+	std::getchar();
 }
