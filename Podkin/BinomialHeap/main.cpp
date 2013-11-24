fea8777c0783d18250a5f3594eb658738f2e88d0
Podkin <alexpodkin@gmail.com>
2013-11-24 22:05:52 +0400

    ihbh

--- Podkin/BinomialHeap/main.cpp
+++ Podkin/BinomialHeap/main.cpp
@@ -1,16 +1,14 @@
 #include "BinomialHeap.h"
+#include "VectorHeap.h"
+#include "IntegrationTest.h"
 #include <functional>
 #include <iostream>
+#include "gtest/gtest.h"
+#include "MyTypeTest.h"
+#include "InvariantTest.h"
 
-int main() {
-    BinomialHeap<int, std::less<int> > *heap = new BinomialHeap<int, std::less<int> >(0);
-    std::cout << heap->getMin();
-    std::cout.flush();
-    BinomialHeap<int, std::less<int> > *heap1 = new BinomialHeap<int, std::less<int> >(1);
-    std::cout << heap1->getMin();
-    std::cout.flush();
-    heap->mergeHeap(heap1);
-    std::cout << heap->getMin();
-    return 0;
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
 
