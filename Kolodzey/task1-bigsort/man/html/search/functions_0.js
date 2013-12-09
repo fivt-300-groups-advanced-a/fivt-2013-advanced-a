eba3fa7cda8e6691498b04eda7b4e2833190b2bb
Daria Kolodzey <kolodaria@google.com>
2013-12-09 12:54:49 +0400

    deleted old tasks, added new notes, added folder for task3

--- Kolodzey/task1-bigsort/man/html/search/functions_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['bigsort',['bigsort',['../namespacebigsort.html#ad69137b3c2dd15604df05fea1809ed17',1,'bigsort']]]
-];
--- Kolodzey/task1-bigsort/man/html/search/functions_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['reversedbubblesort',['reversedBubblesort',['../namespacebigsort.html#aafceedf48ed2c8dfc743408232c7ef07',1,'bigsort']]]
-];
