f17604653cc0355196d5dfbd556952bd2aedc22e
Maxim Rusak <mak-rusak@yandex.ru>
2013-12-12 03:20:13 +0300

    Rusak is the monster of testing.

--- Rusak/project2/heap_test.h
+++ Rusak/project2/heap_test.h
@@ -1,29 +1,49 @@
-
 template<typename T, typename Cmp>
 class HeapTest {
+
+  typedef typename Heap<T, Cmp>::Vertex Vertex_type;
+  typedef Heap<T, Cmp> Heap_type;
   
   public: 
-    bool check_invariants(Heap<T, Cmp> &heap) {
+    bool check_inv_vert(Vertex_type* vert, int deg) {
+      if (!vert) return true;
+      if (vert->degree!=deg) return false;
+      int wd = deg-1;
       bool ok = true;
-      bool was[100];
-      for (Heap<T, Cmp>::Vertex* head = heap.head; ok && head; head = head->sibling) {
-        if (was[head->degree]) ok = false;
-        was[head->degree] = 1;
-        if (head->sibling && head->sibling->degree >= head->degree) ok = false;
-        check_invariants(head, head->degree);
+      for (Vertex_type* fir = vert->child; ok && fir; fir = fir->sibling, wd--) {
+        ok = ok & check_inv_vert(fir, wd) & (!cmp(fir->key, vert->key));
+      }
+      if (!ok) {
+        std::cout << "OH\n";
+        exit(0);
       }
       return ok;
     }
 
-    bool check_invariants(Heap<T, Cmp>::Vertex* vert, int deg) {
-      if (!vert) return true;
-      if (vert->degree!=deg) return true;
-      int wd = deg-1;
+    bool check_inv_heap(Heap_type &heap) {
       bool ok = true;
-      for (Heap<T, Cmp>::Vertex* fir = vert->child; ok && fir; fir = fir->sibling, wd--) {
-        ok = ok && check_invariants(fir, wd) && cmp(vert->key, fir->key);
+      bool was[100];
+      for (int i=0;i<100;i++) was[i] = 0;
+      for (Vertex_type* head = heap.head; ok && head; head = head->sibling) {
+        if (was[head->degree]) ok = false;
+        was[head->degree] = 1;
+        if (head->sibling && head->sibling->degree <= head->degree) ok = false;
+        ok = ok & check_inv_vert(head, head->degree);
       }
       return ok;
+    }
+
+    int size_vert(Vertex_type* head) {
+      if (!head) return 0;
+      int col = 1;
+      for (Vertex_type* f = head->child; f; f = f->sibling) col+=size_vert(f);
+      return col;
+    }
+
+    int size_heap(Heap_type &heap) {
+      int col = 0;
+      for (Vertex_type* head = heap.head; head; head = head->sibling) col+=size_vert(head);
+      return col;
     }
 
     HeapTest(Cmp comp) {
@@ -34,6 +54,7 @@ class HeapTest {
     Cmp cmp;    
 };
 
-typename<typename T, typename Cmp = std::less<T> > MakeHeapTest(Cmp cmp = Cmp()) {
+template<typename T, typename Cmp = std::less<T> > 
+HeapTest<T, Cmp> MakeHeapTest(Cmp cmp = Cmp()) {
   return HeapTest<T, Cmp>(cmp);
 }
--- Rusak/project2/test.cpp
+++ Rusak/project2/test.cpp
@@ -1,30 +1,37 @@
-#include "heap.h"
 #include <gtest/gtest.h>
 #include <vector>
 #include <algorithm>
+#include <iostream>
+#include <ctime>
 
-TEST(UnitTests,Insert_Merge_getMin_extractMin)
-{
+#include "heap.h"
+#include "heap_test.h"
+
+TEST(PrepareForTest, GenerateRand) {
+  srand(time(NULL));
+}
+
+TEST(UnitTests,Insert_Merge_getMin_extractMin) {
 	auto heap1 = MakeHeap<int>();
 	auto heap2 = MakeHeap<int>();
-	int mas[1000];
-	for (int i=0;i<1000;i++)
+  const int n = 300000;
+	int mas[n];
+	for (int i=0;i<n;i++)
 		mas[i]=rand()%10000000;
-	for (int i=0;i<500;i++) {
+	for (int i=0;i<n/2;i++) {
 		heap1.insert(mas[i]);
-		heap2.insert(mas[i+500]);
+		heap2.insert(mas[i+n/2]);
 	}
 	heap1.merge(heap2);
-	std::sort(mas,mas+1000);
-	for (int i=0;i<1000;i++) {
+	std::sort(mas,mas+n);
+	for (int i=0;i<n;i++) {
 		EXPECT_EQ(heap1.find_min(),mas[i]);
 		heap1.extract_min();
 	}
 	EXPECT_TRUE(heap1.empty());
 }
 
-TEST(IntegrationTests, HeapSort)
-{
+TEST(IntegrationTests, HeapSort) {
 	auto heap = MakeHeap<int>();
 	int n=300000;
 	std::vector<int> a(n);
@@ -32,10 +39,77 @@ TEST(IntegrationTests, HeapSort)
 		a[i]=rand();
 	for (int i=0;i<n;i++)
 		heap.insert(a[i]);
+  EXPECT_FALSE(heap.empty());
 	std::sort(a.begin(),a.end());
 	for (int i=0;i<n;i++) {
 		EXPECT_EQ(heap.find_min(),a[i]);
 	  heap.extract_min();
 	}
 	EXPECT_TRUE(heap.empty());
+}
+
+TEST(InsideTests, FewInvariants) {
+  const int n = 4500;
+  auto test = MakeHeapTest<int>();
+  auto heap = MakeHeap<int>();
+  for (int i=0;i<n;i++) {
+    heap.insert(rand()%1000000);
+    EXPECT_TRUE( test.check_inv_heap(heap) );
+    EXPECT_EQ( test.size_heap(heap), i+1);
+  }
+  EXPECT_FALSE(heap.empty());
+  for (int i=0;i<n;i++) {
+    heap.extract_min();
+    EXPECT_TRUE( test.check_inv_heap(heap) );
+    EXPECT_EQ( test.size_heap(heap), n-i-1);
+  }
+  EXPECT_TRUE(heap.empty());
+}
+
+TEST(InsideTests, InvariantsAfterBigMerges) {
+  const int on_each = 30000;
+  const int count = 10;
+  auto test = MakeHeapTest<int>();
+
+  Heap<int, std::less<int> > heap[count];
+  for (int i=0;i<count;i++) heap[i] = MakeHeap<int>();
+  for (int i=0;i<on_each;i++) {
+    for (int j=0;j<count;j++) {
+      heap[j].insert(rand()%1000000);
+    }
+  }
+  for (int j=count-2;j>=0;j--) {
+    heap[j].merge(heap[j+1]);
+    EXPECT_TRUE( test.check_inv_heap(heap[j]) );
+    EXPECT_EQ( test.size_heap(heap[j]), (count-j)*on_each );
+    EXPECT_TRUE(heap[j+1].empty());
+  }
+  EXPECT_FALSE(heap[0].empty());
+}
+
+TEST(InsideTests, StressTest) {
+  const int oper = 6000;
+  int col = 0;
+  auto main = MakeHeap<int>();
+  auto test = MakeHeapTest<int>();
+  for (int op = 0; op<oper;op++) {
+    int type = rand()%2;
+    if (col==0) type = 0;
+    if (type==0) {
+      int add = rand()%10+1;
+      auto added_heap = MakeHeap<int>();
+      for (int j=0;j<add;j++) added_heap.insert(rand()%1000000);
+      EXPECT_TRUE( test.check_inv_heap(added_heap) );
+      EXPECT_EQ( test.size_heap(added_heap), add);
+      main.merge(added_heap);
+      col+=add;
+      EXPECT_TRUE( added_heap.empty() );
+    }
+    else if (type==1) {
+      col--;
+      main.extract_min();
+    }
+    EXPECT_TRUE( test.check_inv_heap(main) );
+    EXPECT_EQ( test.size_heap(main), col);
+  }
 }
