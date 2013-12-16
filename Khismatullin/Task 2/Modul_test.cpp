421ca7bd389f002d8a0592da9ba72c87591def00
Khismatullin <pingshkiper@yandex.ru>
2013-12-16 10:16:15 +0400

    Task 2 completed

--- Khismatullin/Task 2/Modul_test.cpp
+++ Khismatullin/Task 2/Modul_test.cpp
@@ -37,6 +37,15 @@ bool operator == (pair<int, int> a, point b)
 	return (a.first == b.x && a.second == b.y);
 }
 
+TEST(Insert, Manual_test)
+{
+	Cheap<int, less<int> > heap;
+	heap.insert(1);
+	heap.insert(1);
+	heap.insert(1);
+	heap.insert(1);
+}
+
 TEST(Insert, some_big_insert)
 {
 	Cheap<int, less<int> > heap;
@@ -44,6 +53,16 @@ TEST(Insert, some_big_insert)
 	{
 		heap.insert(rand());
 	}
+}
+
+TEST(Get_min, Manual_test)
+{
+	Cheap<int, less<int> > heap;
+	heap.insert(3);
+	heap.insert(2);
+	heap.insert(1);
+	heap.insert(0);
+	EXPECT_EQ(0, heap.get_min());
 }
 
 TEST(Get_min, decreasing_sequence)
@@ -79,6 +98,23 @@ TEST(Get_min, random_sequence)
 	}
 }
 
+TEST(Extract_min, Manual_test)
+{
+	Cheap<int, less<int> > heap;
+	heap.insert(1);
+	heap.insert(7);
+	heap.insert(6);
+	heap.insert(5);
+	EXPECT_EQ(heap.get_min(), 1);
+	heap.extract_min();
+	heap.insert(2);
+	heap.insert(3);
+	heap.insert(4);
+	heap.insert(5);
+	heap.insert(4);
+	EXPECT_EQ(heap.get_min(), 2);
+	heap.extract_min();
+}
 
 TEST(Extract_min, increasing_sequence)
 {
@@ -109,6 +145,27 @@ TEST(Extract_min, random_big_sequence)
 		heap.extract_min();
 		st.erase(st.begin());
 	}
+}
+
+TEST(Merge, Manual_test)
+{
+	Cheap<int, less<int> > heap[3];
+	for(int i = 0; i < 3; i++)
+	{
+		heap[0].insert(i);
+		heap[1].insert(i + 3);
+	}
+	for(int i = 0; i < 7; i++)
+		heap[2].insert(i + 6);
+	heap[0].merge(heap[1]);
+	heap[1].merge(heap[2]);
+	heap[0].merge(heap[1]);
+	for(int i = 0; i < 13; i++)
+	{
+		EXPECT_EQ(heap[0].get_min(), i);
+		heap[0].extract_min();
+	}
+	
 }
 
 TEST(Merge, ten_heaps_modulo_ten)
@@ -153,6 +210,16 @@ TEST(Merge, groups_of_integers)
 		EXPECT_EQ(heap.get_min(), i + 1);
 		
 	}
+}
+
+
+TEST(Clear, Manual_test)
+{
+	Cheap<int, less<int> > heap;
+	heap.insert(1);
+	heap.clear();
+	heap.insert(2);
+	EXPECT_EQ(heap.get_min(), 2);
 }
 
 TEST(Clear, groups)
@@ -215,6 +282,58 @@ TEST(My_Comporator, random_sequence)
 		st.erase(st.begin());
 	}
 	
+}
+
+TEST(Decrease_key, Manual_test)
+{
+	Cheap<int, less<int> > heap;
+	heap.insert(2);
+	heap.insert(2);
+	heap.insert(2);
+	heap.decrease_key(heap.insert(2), 1);
+	EXPECT_EQ(1, heap.get_min());
+}
+
+TEST(Decrease_key, some_big_test)
+{
+	Cheap<int, less<int> > heap;
+	Cheap<int, less<int> > ::index a[1024];
+	for(int i = 0; i < 1024; i++) {
+		a[i] = heap.insert(i + 1024);
+	}
+	for(int i = 1023; i >= 0; i--) {
+		heap.decrease_key(a[i], i);
+	}
+	for(int i = 0; i < 1024; i++) {
+		EXPECT_EQ(heap.get_min(), i);
+		heap.extract_min();
+	}
+}
+
+TEST(Erase, Manual_test)
+{
+	Cheap<int, less<int> > heap;
+	heap.insert(2);
+	heap.insert(2);
+	Cheap<int, less<int> > ::index id = heap.insert(1);
+	heap.erase(id, -2);
+	EXPECT_EQ(2, heap.get_min());
+}
+
+TEST(Decrease_key, some_big_test)
+{
+	Cheap<int, less<int> > heap;
+	Cheap<int, less<int> > ::index a[1024];
+	for(int i = 0; i < 1024; i++) {
+		a[i] = heap.insert(i + 1024);
+	}
+	for(int i = 1023; i >= 0; i--) {
+		heap.decrease_key(a[i], i);
+	}
+	for(int i = 0; i < 1024; i++) {
+		EXPECT_EQ(heap.get_min(), i);
+		heap.extract_min();
+	}
 }
 
 int main(int argc, char* argv[]) {
