cee1cac36fcce2efef23a9be428038583556fc55
kuzmichevdima95 <kuzmichevdima95@gmail.com>
2013-12-21 23:29:32 +0400

    first commit with segment trees

---
+++ Kuzmichev/SegTree/plus_sum_min_max_tree.h
@@ -0,0 +1,50 @@
+#include "return_types.h"
+
+struct MetaPlusSumMinMax
+{
+	int plusedValue;
+	MetaPlusSumMinMax(int _plusedValue) : plusedValue(_plusedValue) {}
+	MetaPlusSumMinMax()
+	{
+		plusedValue = 0;
+	}
+	void clear()
+	{
+		plusedValue = 0;
+	}
+};
+
+
+class MethodsPlusSumMinMax
+{
+public:
+	StructSumMinMax merge(StructSumMinMax L, StructSumMinMax R)
+	{
+		//printf("merge (%d %d)  (%d %d)\n", L.first, L.second, R.first, R.second);
+		return StructSumMinMax(L.sum + R.sum, min(L.min, R.min), max(L.max, R.max));
+	}
+	void apply(MetaPlusSumMinMax & m, StructSumMinMax & p, Seg s)
+	{
+		//s: [L; R)
+		if (m.plusedValue == 0)
+			return;
+		p.sum += m.plusedValue * s.getLength();
+		p.min += m.plusedValue;
+		p.max += m.plusedValue;
+		//p = mp(m.assignedValue * s.getLength(), m.assignedValue);
+		//if (DEBUG2) printf("apply assVal = %d p = (%d %d)\n", m.assignedValue, p.first, p.second);
+	}
+	//void push(MetaAssignSumMin & m, pair <int, int> & p, MetaAssignSumMin & leftMeta, MetaAssignSumMin & rightMeta, int L, int R)
+	void push(MetaPlusSumMinMax & m, MetaPlusSumMinMax & leftMeta, MetaPlusSumMinMax & rightMeta)
+	{
+		//apply(m, p, L, R);
+		
+		if (m.plusedValue != 0)
+		{
+			leftMeta.plusedValue += m.plusedValue;
+			rightMeta.plusedValue += m.plusedValue;
+			//if (DEBUG2) printf("push (%d) -> (%d) (%d)\n", m.assignedValue, leftMeta.assignedValue, rightMeta.assignedValue);	
+		}
+		//MetaAssignSumMin.clear();
+	}
+};
