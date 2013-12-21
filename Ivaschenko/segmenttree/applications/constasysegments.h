04f7a33951565682a32bbb4b15a7c2d5e537384e
Skird <grafoman96@gmail.com>
2013-12-22 02:08:20 +0400

    Changes: little refactoring

--- Ivaschenko/segmenttree/applications/constasysegments.h
+++ Ivaschenko/segmenttree/applications/constasysegments.h
@@ -1,6 +1,11 @@
+#ifndef CONSTASYSEGMENTS_H
+#define CONSTASYSEGMENTS_H
+
 #include <gtest/gtest.h>
 
 TEST(ConstansySegments, Integration)
 {
 	// TODO: count constansy subsegments on a segment
 }
+
+#endif // CONSTASYSEGMENTS_H
