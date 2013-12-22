23c2d5922a4634a22a6d077c3a63f5b6c09af875
Skird <grafoman96@gmail.com>
2013-12-22 17:30:45 +0400

    Changes: 	- MaximalSumSubSegmentTree for any type 	- little piece of test for MaximalSumSubSegmentTree

--- Ivaschenko/segmenttree/applications/constasysegments.h
+++ Ivaschenko/segmenttree/applications/constasysegments.h
@@ -1,11 +1,4 @@
 #ifndef CONSTASYSEGMENTS_H
 #define CONSTASYSEGMENTS_H
 
-#include <gtest/gtest.h>
-
-TEST(ConstansySegments, Integration)
-{
-	// TODO: count constansy subsegments on a segment
-}
-
 #endif // CONSTASYSEGMENTS_H
