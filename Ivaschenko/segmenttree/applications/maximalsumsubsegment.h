426694e24281870d89ddf7605a4f96e387809663
Skird <grafoman96@gmail.com>
2013-12-23 06:48:48 +0400

    Changes: 	- A lot of bugs fixed 	- Working implementation of maximal sum subsegment + stress

--- Ivaschenko/segmenttree/applications/maximalsumsubsegment.h
+++ Ivaschenko/segmenttree/applications/maximalsumsubsegment.h
@@ -7,49 +7,83 @@
 
 template<typename DataType, typename Comparator = std::less<DataType> > class MaximalSumSubSegmentTree
 {
+	private:
+		struct ReturnType;
+
 	public:
 		template<typename ForwardIterator> MaximalSumSubSegmentTree(ForwardIterator begin, ForwardIterator end,
 																	const DataType &nIdentity = DataType(),
 																	Comparator nCmp = Comparator()):
-			cmp(nCmp), identity(nIdentity)
+			cmp(nCmp), identity(nIdentity),
+			tree(1, ReturnType(), Function(cmp), MetaUpdater(cmp, nIdentity)) /* dirty hack */
 		{
-			std::vector< std::pair<DataType, int> > data;
-			for (size_t i = 0; begin != end; ++begin, ++i)
-				data.push_back(std::make_pair(*begin, i));
+			std::vector<ReturnType> data;
+			for (std::size_t i = 0; begin != end; ++begin, ++i)
+			{
+				ReturnType cur;
+				if (!cmp(*begin, identity))
+				{
+					cur.maxPrefix = cur.maxSuffix = cur.maxSubseg = *begin;
+					cur.prefPos = i + 1, cur.suffPos = i;
+					cur.segLeft = i, cur.segRight = i + 1;
+				}
+				else
+				{
+					cur.maxPrefix = cur.maxSuffix = cur.maxSubseg = identity;
+					cur.prefPos = i, cur.suffPos = i + 1;
+					cur.segLeft = i + 1, cur.segRight = i;
+				}
+				cur.sum = *begin;
+				cur.neutral = false;
+				data.push_back(cur);
+			}
 			tree = GeneralSegmentTree<ReturnType, MetaInformation, Function, MetaUpdater, MetaMerger>
-					(data.begin(), data.end(), ReturnType());
+					(data.begin(), data.end(), ReturnType(), Function(cmp), MetaUpdater(cmp, nIdentity));
 		}
 
-		template<typename ForwardIterator> MaximalSumSubSegmentTree(std::size_t n, const DataType &value = DataType(),
-																	const DataType &nIdentity = DataType(),
-																	Comparator nCmp = Comparator()):
-			cmp(nCmp), identity(nIdentity)
+		MaximalSumSubSegmentTree(std::size_t n, const DataType &value = DataType(),
+								 const DataType &nIdentity = DataType(),
+								 Comparator nCmp = Comparator()):
+			cmp(nCmp), identity(nIdentity),
+			tree(1, ReturnType(), Function(cmp), MetaUpdater(cmp, nIdentity)) /* dirty hack */
 		{
-			std::vector< std::pair<DataType, int> > data(n, value);
+			std::vector<ReturnType> data;
+			for (std::size_t i = 0; i < n; ++i)
+			{
+				ReturnType cur;
+				cur.maxPrefix = cur.maxSuffix = cur.maxSubseg = identity;
+				cur.prefPos = i, cur.suffPos = i + 1;
+				cur.segLeft = i + 1, cur.segRight = i;
+				cur.neutral = false;
+				data.push_back(cur);
+			}
 			tree = GeneralSegmentTree<ReturnType, MetaInformation, Function, MetaUpdater, MetaMerger>
-					(data.begin(), data.end(), ReturnType());
+					(data.begin(), data.end(), ReturnType(), Function(cmp), MetaUpdater(cmp, nIdentity));
+		}
+
+		void update(std::size_t left, std::size_t right, const DataType &value)
+		{
+			tree.update(left, right, MetaInformation(value));
+		}
+
+		struct Segment
+		{
+			public:
+				std::size_t left, right;
+				DataType sum;
+
+				Segment(const ReturnType &ans): left(ans.segLeft), right(ans.segRight), sum(ans.maxSubseg) {}
+		};
+
+		Segment maximalSubsegment(std::size_t left, std::size_t right)
+		{
+			return Segment(tree.get(left, right));
 		}
 
 	private:
 		struct ReturnType
 		{
 			public:
-				ReturnType(const std::pair<DataType, int> &p): sum(p.first), neutral(false)
-				{
-					if (!cmp(sum, identity))
-					{
-						maxPrefix = maxSuffix = maxSubseg = sum;
-						prefPos = p.second + 1, suffPos = p.second;
-						segLeft = p.second + 1, segRight = p.second;
-					}
-					else
-					{
-						maxPrefix = maxSuffix = maxSubseg = 0;
-						prefPos = p.second, suffPos = p.second + 1;
-						segLeft = p.second, segRight = p.second + 1;
-					}
-				}
-
 				ReturnType(): neutral(true) {}
 
 				DataType sum, maxPrefix, maxSuffix, maxSubseg;
@@ -70,65 +104,82 @@ template<typename DataType, typename Comparator = std::less<DataType> > class Ma
 		struct Function
 		{
 			public:
-				void operator () (ReturnType &a, const ReturnType &b)
+				Function(Comparator nCmp): cmp(nCmp) {}
+
+				ReturnType operator () (const ReturnType &a, const ReturnType &b)
 				{
-					if (b.neutral) return;
-					if (a.neutral || cmp(a.maxSubseg, b.maxSubseg))
+					ReturnType result = a;
+					result.maxSuffix = b.maxSuffix;
+					result.suffPos = b.suffPos;
+
+					if (b.neutral) return result;
+					if (result.neutral || cmp(result.maxSubseg, b.maxSubseg))
 					{
-						a.maxSubseg = b.maxSubseg;
-						a.segLeft = b.segLeft;
-						a.segRight = b.segRight;
+						result.maxSubseg = b.maxSubseg;
+						result.segLeft = b.segLeft;
+						result.segRight = b.segRight;
 					}
-					if (cmp(a.maxSubseg, a.maxSuffix + b.maxPrefix))
+					if (cmp(result.maxSubseg, a.maxSuffix + b.maxPrefix))
 					{
-						a.maxSubseg = a.maxSuffix + b.maxSuffix;
-						a.segLeft = a.suffPos; // inclusive suffix
-						a.segRight = b.prefPos; // exclusive prefix
+						result.maxSubseg = a.maxSuffix + b.maxPrefix;
+						result.segLeft = a.suffPos; // inclusive suffix
+						result.segRight = b.prefPos; // exclusive prefix
 					}
 
-					if (a.neutral || cmp(a.maxPrefix, b.maxPrefix + a.sum))
+					if (result.neutral || cmp(result.maxPrefix, a.sum + b.maxPrefix))
 					{
-						a.maxPrefix = b.maxPrefix + a.sum;
-						a.prefPos = b.prefPos; // exclusive prefix
+						result.maxPrefix = a.sum + b.maxPrefix;
+						result.prefPos = b.prefPos; // exclusive prefix
 					}
 
-					if (a.neutral || cmp(a.maxSuffix, a.maxSuffix))
+					if (result.neutral || cmp(result.maxSuffix, a.maxSuffix + b.sum))
 					{
-						a.maxSuffix = a.maxSuffix + b.sum;
-						a.suffPos = a.suffPos; // Unchanged! (inclusive)
+						result.maxSuffix = a.maxSuffix + b.sum;
+						result.suffPos = a.suffPos; // Unchanged! (inclusive)
 					}
-					a.neutral = false;
-					a.sum += b.sum;
+					result.neutral = false;
+					result.sum = (result.neutral ? 0 : result.sum) + b.sum;
+					return result;
 				}
+
+			private:
+				Comparator cmp;
 		};
 
 		struct MetaUpdater
 		{
 			public:
+				MetaUpdater(Comparator nCmp, const DataType &nIdentity): cmp(nCmp), identity(nIdentity) {}
+
 				void operator () (ReturnType &value, const MetaInformation &info,
 								  std::size_t left, std::size_t right)
 				{
+					if (!info.assigned) return;
 					value.sum = (right - left + 1) * info.value;
 
 					if (cmp(info.value, identity))
 					{
-						value.maxPrefix = value.maxSuffix = value.maxSubseg = DataType(0);
+						value.maxPrefix = value.maxSuffix = value.maxSubseg = identity;
 						value.prefPos = left, value.suffPos = right + 1;
-						value.segLeft = left, value.segRight = right + 1;
+						value.segLeft = right + 1, value.segRight = left;
 					}
 					else
 					{
 						value.maxPrefix = value.maxSuffix = value.maxSubseg = value.sum;
 						value.prefPos = right + 1, value.suffPos = left;
-						value.segLeft = -1, value.segRight = -1;
+						value.segLeft = left, value.segRight = right + 1;
 					}
 				}
+
+			private:
+				Comparator cmp;
+				DataType identity;
 		};
 
 		struct MetaMerger
 		{
 			public:
-				void operator() (MetaInformation &a, const MetaInformation &b)
+				void operator() (MetaInformation &a, const MetaInformation &b, std::size_t, std::size_t)
 				{
 					if (b.assigned) a = b;
 				}
