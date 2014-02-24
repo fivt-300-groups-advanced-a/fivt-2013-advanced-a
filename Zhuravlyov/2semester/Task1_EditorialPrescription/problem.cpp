38f9706ab841e76cb9b8420efe86f57b074cbbc9
Alexey <aa_Zhur@mail.com>
2014-02-24 11:59:31 +0400

    Some changes in first task
    Former-commit-id: 2b25a0578071fabb8fe827637eeb5466b260fa0c

--- Zhuravlyov/2semester/Task1_EditorialPrescription/problem.cpp
+++ Zhuravlyov/2semester/Task1_EditorialPrescription/problem.cpp
@@ -1,5 +1,6 @@
 #include <string>
 #include <vector>
+#include <cassert>
 
 
 
@@ -7,11 +8,7 @@ void get_result(const std::string& s, const std::string&t,
 			   int s_first, int s_last, int t_first, int t_last, 
 			   std::vector<std::string>& list)
 {
-	if (s_first == s_last)
-	{
-		for (int i = t_last - 1; i >= t_first; i--)
-			list.push_back("delete " + std::to_string((long long)i) + " symbol");
- 	} else if (t_first == t_last)
+	if (t_first == t_last)
 	{
 		for (int i = s_last - 1; i >= s_first; i--)
 		{
@@ -20,10 +17,22 @@ void get_result(const std::string& s, const std::string&t,
 			notation = notation + " to " + std::to_string((long long) t_first) + " position";
 			list.push_back(notation);
 		}
-	} else if (s_last - s_first == 1 && t_last - t_first == 1)
+	} else
+	if (s_first == s_last - 1)
 	{
-		if (s[s_first] != t[t_first])
-			list.push_back("change " + std::to_string((long long) t_first) + " symbol to " + s[s_first]);
+			bool flag = false;
+			for (int i = t_last-1; i>=t_first; i--)
+				if (flag || t[i] != s[s_first]) 
+				{
+					list.push_back("delete " + std::to_string((long long)i) + " symbol");
+				} else if (t[i] == s[s_first]) 
+					flag = true;
+
+			if (!flag) 
+			{
+				list.pop_back();
+				list.push_back("change "+std::to_string((long long) t_first) + " symbol to " + s[s_first]);
+			}
 	} else
 	{
 		int t_len = t_last - t_first + 1;
@@ -33,6 +42,7 @@ void get_result(const std::string& s, const std::string&t,
 
 		for (int i = 0; i < t_len; i++)
 			tmp_dp_left[i] = dp_left[i] = i;
+
 		int mid_s = s_len / 2;
 		for (int i = 1; i <= mid_s; i++)
 		{
@@ -41,9 +51,10 @@ void get_result(const std::string& s, const std::string&t,
 			{
 				if (s[s_first + i - 1] == t[t_first + j - 1])
 					dp_left[j] = tmp_dp_left[j - 1];
-				else dp_left[j] = 1 + std::min(std::min(tmp_dp_left[j],tmp_dp_left[j - 1]), dp_left[j - 1]);
+				else dp_left[j] = 1 + std::min(std::min(tmp_dp_left[j], tmp_dp_left[j - 1]), dp_left[j - 1]);
 			}
-			tmp_dp_left = dp_left;
+			if (i < mid_s)
+				tmp_dp_left.swap(dp_left);
 		}
 
 		for (int i = 0; i < t_len; i++)
@@ -56,30 +67,26 @@ void get_result(const std::string& s, const std::string&t,
 			{
 				if (s[s_first + i - 1] == t[t_first + j])
 					dp_right[j] = tmp_dp_right[j + 1];
-				else dp_right[j] = 1 + std::min(std::min(tmp_dp_right[j],tmp_dp_right[j + 1]), dp_right[j + 1]);
+				else dp_right[j] = 1 + std::min(std::min(tmp_dp_right[j], tmp_dp_right[j + 1]), dp_right[j + 1]);
 			}
-			tmp_dp_right = dp_right;
+			if ( i > mid_s + 1)
+				tmp_dp_right.swap(dp_right);
 		}
 
 		int min = t_len + s_len;
 		int mid_t = 0;
 		for (int i = 0; i < t_len; i++)
-			if (dp_left[i] + dp_right[i] < min) {
+			if (dp_left[i] + dp_right[i] < min) 
+			{
 				min = dp_left[i] + dp_right[i];
 				mid_t = i;
 			}
 		mid_t += t_first;
 		mid_s += s_first;
-		if (s_first == s_last - 1 && mid_t == t_last)
-		{
-			bool flag = false;
-			for (int i = t_last-1; i>=t_first; i--)
-				if (flag || t[i] != s[s_first]) {
-					list.push_back("delete " + std::to_string((long long)i) + " symbol");
-				} else if (t[i] == s[s_first]) 
-					flag = true;
-			return;
-		}
+
+		dp_left.clear(); dp_right.clear();
+		tmp_dp_left.clear(); tmp_dp_right.clear();
+
 		get_result(s, t, mid_s, s_last, mid_t, t_last, list);
 		get_result(s, t, s_first, mid_s, t_first, mid_t, list);
 	}
