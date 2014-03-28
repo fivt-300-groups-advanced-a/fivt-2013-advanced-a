d8a13edaab59a883b8735fcd43ecc692251e0e22
pershik <k.pershik@gmail.com>
2014-03-29 01:19:53 +0400

    Graph added
    Former-commit-id: 581480042838563d9923c46fc72da2bff3c5b5a4

---
+++ Pershakov/graph/graph.h
@@ -0,0 +1,24 @@
+#ifndef GRAPH_H
+
+#define GRAPH_H
+
+#include <vector>
+
+#include "list_of_incidents.h"
+
+class Graph {
+    public:
+       Graph(std::vector<std::unique_ptr<ListOfIncidents> > &_g) {
+           g = move(_g);
+       }
+
+       const ListOfIncidents& getIncidents(int v) {
+           return *g[v];
+       }
+
+
+    private:
+        std::vector<std::unique_ptr<ListOfIncidents> > g;
+};
+
+#endif
