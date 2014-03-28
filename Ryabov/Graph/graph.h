3f4247599cb7d0a77b8e4355aa118c3ad19e7003
Mark Ryabov <mir@epage.ru>
2014-03-28 00:30:17 +0000

    Today's final build
    Former-commit-id: f7316074a5f6ed8bc7a4ced8988b8bdd890a8f8e

--- Ryabov/Graph/graph.h
+++ Ryabov/Graph/graph.h
@@ -1,10 +1,57 @@
 #ifndef MGRAPH
 #define MGRAPH
 
+#include <vector>
+#include <memory>
+#include <stdlib.h>
+
+
+
+class ListOfIncidents
+{
+public:
+    class Iterator
+    {
+    public:
+        virtual const Iterator& operator*() const = 0;
+        virtual Iterator& operator++ () = 0;
+    };
+
+    virtual Iterator* begin() = 0;
+    virtual bool isConnected(size_t) const = 0;
+    virtual void addEdge(size_t) = 0;
+    virtual void removeEdge(size_t) = 0;
+    virtual void removeEdge(Iterator *it) = 0;
+};
+
+class Factory
+{
+public:
+    virtual ListOfIncidents* getList() = 0;
+};
 class Graph
 {
 public:
     void print();
+    std::vector<ListOfIncidents*> vertexes;
+    
+    Graph(Factory* factory) : factory(factory)
+    {
+        
+    }
+
+    Graph(Factory* factory, size_t n) : factory(factory)
+    {
+        vertexes.assign(n, NULL);
+        for (size_t i = 0; i < n; ++i)
+            vertexes[i] = factory->getList();
+    }
+    Graph()
+    { }
+private:
+    Factory *factory;
 };
+
+
 
 #endif
