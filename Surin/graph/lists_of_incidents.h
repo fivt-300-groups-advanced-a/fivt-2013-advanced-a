d6d02d360c60e4d13e152147f6323e54b62233e1
mike <surinmike@gmail.com>
2014-05-05 00:00:03 +0400

    const
    Former-commit-id: 2ea9002eb2e9db4cb1fd34344b9a2d63f19bc41f

--- Surin/graph/lists_of_incidents.h
+++ Surin/graph/lists_of_incidents.h
@@ -10,8 +10,8 @@ class VectorIntList : public ListOfIncedents {
         class Iterator: public IntIterator {
             private:
                 friend VectorIntList;
-                std::vector<int>::iterator it, end_it;
-                Iterator(std::vector<int>::iterator it, std::vector<int>::iterator end_it)
+                std::vector<int>::const_iterator it, end_it;
+                Iterator(std::vector<int>::const_iterator it, std::vector<int>::const_iterator end_it)
                     : it(it), end_it(end_it) {
                 }
             public:
@@ -29,11 +29,11 @@ class VectorIntList : public ListOfIncedents {
             std::sort(v.begin(), v.end());
             _v.resize(std::unique(v.begin(), v.end()) - v.begin());
         }
-        virtual std::unique_ptr<IntIterator> begin() {
+        virtual std::unique_ptr<IntIterator> begin() const {
             return std::unique_ptr<IntIterator>((IntIterator *)(new Iterator(v.begin(), v.end())));
         }
-        virtual bool isConnected(int x) {
-            std::vector<int>::iterator it = std::lower_bound(v.begin(), v.end(), x);
+        virtual bool isConnected(int x) const {
+            std::vector<int>::const_iterator it = std::lower_bound(v.cbegin(), v.cend(), x);
             if (it == v.end()) return false;
             return *it == x;
         }
@@ -47,8 +47,8 @@ class VectorBoolList : public ListOfIncedents {
             private:
                 friend VectorBoolList;
                 int it;
-                std::vector<bool> & v;
-                Iterator(std::vector<bool> & v): v(v), it(0) {
+                const std::vector<bool> & v;
+                Iterator(const std::vector<bool> & v): v(v), it(0) {
                     while (it < v.size() && !v[it]) {
                         it++;
                     }
@@ -68,10 +68,10 @@ class VectorBoolList : public ListOfIncedents {
         };
         VectorBoolList(std::vector<bool> && _v): v(_v) {
         }
-        virtual std::unique_ptr<IntIterator> begin() {
+        virtual std::unique_ptr<IntIterator> begin() const {
             return std::unique_ptr<IntIterator>((IntIterator*) new Iterator(v));
         }
-        virtual bool isConnected(int x) {
+        virtual bool isConnected(int x) const {
             return v[x];
         }
 };
@@ -100,10 +100,10 @@ class OneList : public ListOfIncedents {
         };
         OneList(int val): val(val) {
         }
-        virtual std::unique_ptr<IntIterator> begin() {
+        virtual std::unique_ptr<IntIterator> begin() const {
             return std::unique_ptr<IntIterator>((IntIterator*) new Iterator(val));
         }
-        virtual bool isConnected(int x) {
+        virtual bool isConnected(int x) const {
             return val == x;
         }
 };
@@ -139,10 +139,10 @@ class FunctionalList: ListOfIncedents {
         };
         FunctionalList(std::function<bool(int)> pred, int n): pred(std::move(pred)), n(n) {
         }
-        virtual std::unique_ptr<IntIterator> begin() {
+        virtual std::unique_ptr<IntIterator> begin() const {
             return std::unique_ptr<IntIterator>(new Iterator(pred, n));
         }
-        virtual bool isConnected(int x) {
+        virtual bool isConnected(int x) const {
             return pred(x);
         }
 };
