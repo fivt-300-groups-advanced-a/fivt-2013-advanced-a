b3c9ce0b6a3ac31e829bcba9640020104ed635dc
pershik <k.pershik@gmail.com>
2014-05-10 02:22:00 +0400

    Added more tests. Seems to be finished
    Former-commit-id: d0da731860f541b19c44accc742d0d69e0d1a030

--- Pershakov/dijkstra/test_dijkstra.py
+++ Pershakov/dijkstra/test_dijkstra.py
@@ -20,6 +20,18 @@ class StringEdge:
         self.label = label
 
 
+class Path:
+    def __init__(self, path, weight=0):
+        self.weight = weight
+        self.path = path
+
+
+def add_edge_to_path(path, edge):
+    pth = path.path[:]
+    pth.append(edge.to)
+    return Path(pth, edge.weight + path.weight)
+
+
 class IntegrationTest(TestCase):
     def test_manual_bamboo(self):
         graph = [[Edge(1, 1)], [Edge(2, 2)], [Edge(3, 3)], []]
@@ -47,6 +59,101 @@ class IntegrationTest(TestCase):
         self.assertEqual(res, [6, 7, 6, 0, 4])
         res = solver.get_shortest_paths(graph, from_vertex=4, init_path_value=0)
         self.assertEqual(res, [2, 3, 6, 4, 0])
+
+    def floyd(self, matrix, size):
+        for k in range(0, size):
+            for i in range(0, size):
+                for j in range(0, size):
+                    matrix[i][j] = min(matrix[i][j], matrix[i][k] + matrix[k][j])
+                    if matrix[i][j] > self.inf // 2:
+                        matrix[i][j] = self.inf
+        for i in range(0, size):
+            for j in range(0, size):
+                if matrix[i][j] == self.inf:
+                    matrix[i][j] = None
+
+    def test_default_realisation(self):
+        """
+        Check algorithm on all undirected graphs with 5 vertexes with random weights
+        """
+        size = 5
+        self.inf = 1000000
+        random.seed(19121994)
+        solver = Dijkstra()
+        for msk in range(0, 1 << (size * (size - 1) // 2)):
+            graph = []
+            for i in range(0, size):
+                graph.append([])
+            matrix = []
+            for i in range(0, size):
+                matrix.append([self.inf] * size)
+            cur = 0
+            for i in range(0, size):
+                matrix[i][i] = 0
+                for j in range(i + 1, size):
+                    weight = random.randint(0, 20)
+                    if msk & (1 << cur):
+                        matrix[i][j] = matrix[j][i] = weight
+                        graph[i].append(Edge(j, weight))
+                        graph[j].append(Edge(i, weight))
+                    cur += 1
+
+            self.floyd(matrix, size)
+            for i in range(0, size):
+                res = solver.get_shortest_paths(graph, from_vertex=i, init_path_value=0)
+                self.assertEqual(res, matrix[i])
+
+    def check_path(self, matrix, from_vertex, to_vertex, path):
+        path_len = len(path.path)
+        if path.path[0] != from_vertex or path.path[path_len - 1] != to_vertex:
+            return False
+        weight = 0
+        for i in range(0, path_len - 1):
+            if matrix[path.path[i]][path.path[i + 1]] is None:
+                return False
+            weight += matrix[path.path[i]][path.path[i + 1]]
+        return weight == path.weight
+
+    def test_not_default_realisation(self):
+        """
+        Find shortest paths to every vertex for all undirected graphs.
+        Their length are checked in previous test yet.
+        """
+        size = 5
+        self.inf = 1000000
+        random.seed(19121994)
+        solver = Dijkstra()
+        for msk in range(0, 1 << (size * (size - 1) // 2)):
+            graph = []
+            for i in range(0, size):
+                graph.append([])
+            matrix = []
+            for i in range(0, size):
+                matrix.append([self.inf] * size)
+            cur = 0
+            for i in range(0, size):
+                matrix[i][i] = 0
+                for j in range(i + 1, size):
+                    weight = random.randint(0, 20)
+                    if msk & (1 << cur):
+                        matrix[i][j] = matrix[j][i] = weight
+                        graph[i].append(Edge(j, weight))
+                        graph[j].append(Edge(i, weight))
+                    cur += 1
+
+            for i in range(0, size):
+                for j in range(0, size):
+                    if matrix[i][j] == self.inf:
+                        matrix[i][j] = None
+
+            for i in range(0, size):
+                res = solver.get_shortest_paths(graph, from_vertex=i, init_path_value=Path([i]),
+                                                cmp=lambda path1, path2: path1.weight < path2.weight,
+                                                add_edge=add_edge_to_path,
+                                                get_destination=lambda edge: edge.to)
+                for j in range(0, size):
+                    if not res[j] is None:
+                        self.assertTrue(self.check_path(matrix, from_vertex=i, to_vertex=j, path=res[j]))
 
 
 class TestRelaxEdge(TestCase):
@@ -120,14 +227,13 @@ class TestRelaxEdge(TestCase):
 class TestGetMinVertex(TestCase):
     def test_default_realisation(self):
         solver = Dijkstra()
-        size = 3
-        cnt_tests = 10
+        size = 4
         max_val = 10
-        for test_number in range(0, cnt_tests):
-            for d1 in range(0, max_val):
-                for d2 in range(0, max_val):
-                    for d3 in range(0, max_val):
-                        solver.dist = [d1, d2, d3]
+        for d1 in range(0, max_val):
+            for d2 in range(0, max_val):
+                for d3 in range(0, max_val):
+                    for d4 in range(0, max_val):
+                        solver.dist = [d1, d2, d3, d4]
                         solver.pr_queue = PriorityQueue()
                         for i in range(0, size):
                             solver.pr_queue.put(PQEntry(solver.dist[i], i))
@@ -142,4 +248,34 @@ class TestGetMinVertex(TestCase):
                             self.assertEqual(solver.dist[min_vertex], solver.dist[pos_min])
                             solver.dist[min_vertex] = max_val + 100
                         min_vertex = solver.get_min_vertex()
+                        self.assertEqual(min_vertex, -1)
+
+    def test_not_default_realisation(self):
+        """
+            Minimum element - string which correspond to the smallest number
+
+        """
+        solver = Dijkstra()
+        cmp = lambda weight1, weight2: int(weight1) < int(weight2)
+        size = 4
+        max_val = 11
+        for d1 in range(0, max_val):
+            for d2 in range(0, max_val):
+                for d3 in range(0, max_val):
+                    for d4 in range(0, max_val):
+                        solver.dist = [str(d1), str(d2), str(d3), str(d4)]
+                        solver.pr_queue = PriorityQueue()
+                        for i in range(0, size):
+                            solver.pr_queue.put(PQEntry(solver.dist[i], i, cmp))
+                            solver.pr_queue.put(PQEntry(str(int(solver.dist[i]) + 2), i, cmp))
+                        for i in range(0, size):
+                            min_vertex = solver.get_min_vertex(cmp)
+                            pos_min = 0
+                            for j in range(0, size):
+                                if cmp(solver.dist[j], solver.dist[pos_min]):
+                                    pos_min = j
+                            self.assertNotEqual(min_vertex, -1)
+                            self.assertEqual(solver.dist[min_vertex], solver.dist[pos_min])
+                            solver.dist[min_vertex] = str(int(max_val) + 100)
+                        min_vertex = solver.get_min_vertex(cmp)
                         self.assertEqual(min_vertex, -1)
