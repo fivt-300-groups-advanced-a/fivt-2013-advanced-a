67a79ab74f29e6e475baac7f37045c911406c3e3
Alex Podkin <alexpodkin@gmail.com>
2014-05-11 17:15:59 +0400

    first version with tests
    Former-commit-id: 4fb5d92c3a7425ca61b99f44f02ed1a36b17277d

---
+++ Podkin/Graph/Graph/AddEdgesToSC.h
@@ -0,0 +1,14 @@
+//
+//  AddEdgesToSC.h
+//  Graph
+//
+//  Created by Алексей Подкин on 27.04.14.
+//  Copyright (c) 2014 Алексей Подкин. All rights reserved.
+//
+
+#ifndef Graph_AddEdgesToSC_h
+#define Graph_AddEdgesToSC_h
+
+
+
+#endif
