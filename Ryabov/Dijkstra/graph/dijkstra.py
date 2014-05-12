fa6e2da63f1928723f717c770a415547d8d2fc48
Mark Ryabov <mir@epage.ru>
2014-05-12 10:01:04 +0000

    Dijkstra
    Former-commit-id: 3ba09984c19e3d401a2cee8e36e9e4f1ef869062

---
+++ Ryabov/Dijkstra/graph/dijkstra.py
@@ -0,0 +1,13 @@
+def dijkstra(g, start, makelen= (lambda a, b : a + b[1]), getvertex= (lambda a : a[0]), init=0, key=lambda a:a):
+    res =  {start : init}
+    used = set()
+    while len(res) > len(used):
+        v = min([(w, q) for q, w in res.items() if q not in used], key=lambda a : key(a[0]))[1]
+        used.add(v)
+        for e in g[v]:
+            u = getvertex(e)
+            if u in res.keys():
+                res[u] = min(res[u], makelen(res[v], e), key=key)
+            else:
+                res[u] = makelen(res[v], e)
+    return res
