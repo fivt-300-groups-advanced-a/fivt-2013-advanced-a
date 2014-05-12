3549462d70fd5f6f29f9bd8ddebc2547c93184ae
Maxim Rusak <mak-rusak@yandex.ru>
2014-05-12 09:59:40 +0400

    graph
    Former-commit-id: 11837659ca848cfa101c6cee1cd941c13dec0c11

--- Rusak/project4/lists_of_incidents.h
+++ Rusak/project4/lists_of_incidents.h
@@ -3,6 +3,7 @@
 
 #include <memory>
 #include <algorithm>
+#include <vector>
 
 class ListIterator {
   public: 
@@ -18,7 +19,6 @@ class ListOfIncidents {
     ListOfIncidents() {}
     virtual std::unique_ptr<ListIterator> get_iterator() const = 0;
     virtual bool is_connected(unsigned int vertex) const = 0;
-    virtual void optimize() = 0;
     virtual ~ListOfIncidents() {}
 };
 
@@ -43,7 +43,7 @@ class BitsetIterator : public ListIterator {
         begin++;
         position++;
       }
-      while (!((*begin) == true) && !(begin==end));
+      while ((*begin)==false && begin!=end);
       if (begin==end) return false;
       return true;
     }
@@ -86,7 +86,6 @@ class VectorIterator : public ListIterator {
 
   private:
     Iterator begin, end;
-    int position;
 };
 
 
@@ -96,27 +95,19 @@ class BitsetList : public ListOfIncidents {
       int sz = *std::max_element(list.begin(), list.end());
       incidents.resize(sz+1);
       for (int i=0;i<list.size();i++) incidents[list[i]] = true;
-      optimized = true;
     }
 
     BitsetList(const std::vector<bool> &list) {
       incidents = list; 
-      optimized = false;
+      unsigned int last = -1;
+      for (int pos=0;pos<(int)incidents.size();pos++)
+        if (incidents[pos]) last = pos;
+      incidents.resize(last+1);
     }
 
     virtual std::unique_ptr<ListIterator> get_iterator() const override {
       std::unique_ptr<ListIterator> get( new BitsetIterator<std::vector<bool>::const_iterator >(incidents.begin(), incidents.end() ) );
       return std::move(get);
-    }
-
-    virtual void optimize() override {
-      if (!optimized) {
-        unsigned int last = -1;
-        for (int pos=0;pos<(int)incidents.size();pos++)
-          if (incidents[pos]) last = pos;
-        incidents.resize(last+1);
-        optimized = true;
-      }
     }
 
     virtual bool is_connected(unsigned int vertex) const override {
@@ -128,20 +119,18 @@ class BitsetList : public ListOfIncidents {
 
   private:
     std::vector<bool> incidents;
-    bool optimized;
 };
 
 class VectorList : public ListOfIncidents {
   public: 
-    VectorList(const std::vector<unsigned int> &list) {
+    VectorList(const std::vector<unsigned int> &list, bool sorted = false) {
       incidents = list;
-      optimized = false;
+      if (!sorted) std::sort(incidents.begin(), incidents.end());
     }
 
     VectorList(const std::vector<bool> &list) {
       for (int pos=0;pos<(int)list.size();pos++)
         if (list[pos]) incidents.push_back(pos);
-      optimized = true;
     }
 
     virtual std::unique_ptr<ListIterator> get_iterator() const override {
@@ -149,27 +138,14 @@ class VectorList : public ListOfIncidents {
       return std::move(get);
     }
 
-    virtual void optimize() override {
-      if (!optimized) {
-        std::sort(incidents.begin(), incidents.end());
-        optimized = true;
-      }
-    }
-
     virtual bool is_connected(unsigned int vertex) const override {
-      if (optimized) {
-        return std::binary_search(incidents.begin(), incidents.end(), vertex);
-      }
-      else {
-        return std::find(incidents.begin(), incidents.end(), vertex) != incidents.end();
-      }
+      return std::binary_search(incidents.begin(), incidents.end(), vertex);
     }
 
     virtual ~VectorList() {}
 
   private:
     std::vector<unsigned int> incidents;
-    bool optimized;
 };
 
 
