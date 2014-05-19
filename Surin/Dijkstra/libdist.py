1c0d65a61a91ff0e821a76227c6715afc4ab5210
mike <surinmike@gmail.com>
2014-05-19 09:05:36 +0400

    improved libdist
    Former-commit-id: 7111e0b7cebe3adbae8ec7a2f86f6c4fa2b09eae

--- Surin/Dijkstra/libdist.py
+++ Surin/Dijkstra/libdist.py
@@ -13,6 +13,7 @@ class Town:
         self.graph[u] += ((tb, te), v)
 
     def __calc__(self):
+        if self.__calc is not None: return
         self.__calc = Dijkstra(self.graph)
         def recalc(a, io):
             (tin, tout) = io
@@ -21,10 +22,13 @@ class Town:
         self.__calc = Dijkstra(self.graph, start=self.start, path_rec=recalc)
 
     def getPath(self, v):
+        self.__calc__()
         return self.__calc.getShortestPath(v)
 
     def getTimes(self):
+        self.__calc__()
         return self.getDistList()
 
     def getTime(self, v):
+        self.__calc__()
         return self.getTime(v)
