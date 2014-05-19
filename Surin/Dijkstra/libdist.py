7232801e572d5437d95f9111f34ccde056ae0f0f
mike <surinmike@gmail.com>
2014-05-19 12:19:55 +0400

    added comments in libdist
    Former-commit-id: 2db136ddd97958ba799b5a41bdde2d47f99b8722

--- Surin/Dijkstra/libdist.py
+++ Surin/Dijkstra/libdist.py
@@ -2,33 +2,40 @@ from dijkstra import Dijkstra
 
 inf = 10**10
 
-class Town:
 
-    def __init__(self, n, start):
+class Transport:
+
+    def __init__(self, n):
+        """
+        :param n: quantity of stations
+        """
         self.graph = [[] for i in range(n)]
         self.__calc = None
-        self.start = start
 
     def addEdge(self, u, v, tb, te):
-        self.graph[u] += ((tb, te), v)
+        """
+        adds edge from u in time tb and arrives v in time te
+        """
+        self.graph[u] += [((tb, te), v)]
 
-    def __calc__(self):
-        if self.__calc is not None: return
-        self.__calc = Dijkstra(self.graph)
-        def recalc(a, io):
-            (tin, tout) = io
-            if tin > a: return inf
-            else: return tout
-        self.__calc = Dijkstra(self.graph, start=self.start, path_rec=recalc)
+    def calc(self, start, start_time=0):
+        def add_edge(time, edge):
+            (tin, tout) = edge
+            if tin < time:
+                return inf
+            else:
+                return tout
+        self.__calc = Dijkstra(self.graph, start=start, path_rec=add_edge, init_id=start_time)
 
     def getPath(self, v):
-        self.__calc__()
         return self.__calc.getShortestPath(v)
 
     def getTimes(self):
-        self.__calc__()
-        return self.getDistList()
+        res = {}
+        for (k, v) in self.__calc.dists.items():
+            if v != inf:
+                res[k] = v
+        return res
 
     def getTime(self, v):
-        self.__calc__()
         return self.getTime(v)
--- Surin/Dijkstra/tests.py
+++ Surin/Dijkstra/tests.py
@@ -1,6 +1,6 @@
 #!/bin/env python3
 from dijkstra import Dijkstra
-from unittest import TestCase, main
+from unittest import TestCase
 from random import Random
 
 
@@ -17,18 +17,16 @@ class IntegrationTest(TestCase):
         ans = Dijkstra(graph)
         self.assertEqual(ans.getDistsList(), [0, 1, 5, 7])
 
-    def test_str_edges(self):
-        graph = [[("a", 1), ("b", 2)], [("c", 3)], [("c", 3)], []]
-        self.assertEqual("ac", Dijkstra(graph, start=0, init_id="").getDistance(3))
-
     def test_floyd(self):
         n = 3
         graph = (((978, 0), (883, 1), (970, 2)), ((869, 0), (57, 1), (93, 2)), ((86, 0), (369, 1), (855, 2)))
         dist = [[10**9] * n for i in range(n)]
         for i in range(n):
             for j in range(n):
-                if i == j: dist[i][j] = 0
-                else: dist[i][j] = graph[i][j][0]
+                if i == j:
+                    dist[i][j] = 0
+                else:
+                    dist[i][j] = graph[i][j][0]
         for i in range(n):
             for j in range(n):
                 for k in range(n):
@@ -49,8 +47,10 @@ class IntegrationTest(TestCase):
         dist = [[10**9] * n for i in range(n)]
         for i in range(n):
             for j in range(n):
-                if i == j: dist[i][j] = 0
-                else: dist[i][j] = graph[i][j][0]
+                if i == j:
+                    dist[i][j] = 0
+                else:
+                    dist[i][j] = graph[i][j][0]
         for i in range(n):
             for j in range(n):
                 for k in range(n):
@@ -101,6 +101,7 @@ class MaxEdgesTest(TestCase):
 
 from copy import deepcopy
 
+
 class Ordinal:
 
     def __init__(self, lst):
@@ -122,7 +123,8 @@ class Ordinal:
         return self.lst < other.lst
 
     def __add__(self, other):
-        if len(other.lst) == 0: return self
+        if len(other.lst) == 0:
+            return self
         lst = deepcopy(self.lst)
         while len(lst) > 0 and lst[-1][0] < other.lst[0][0]:
             lst.pop()
@@ -141,53 +143,53 @@ def rand_ord(max_v):
 
 class OrdinalTest(TestCase):
 
-    def test_Comparsion(self):
-        a = Ordinal([(1, 2), (0, 1)])  # w * 2 + 1
-        b = Ordinal([(1, 1), (0, 1)])  # w + 1
+    def test_cmp(self):
+        a = Ordinal([(1, 2), (0, 1)])
+        b = Ordinal([(1, 1), (0, 1)])
         self.assertGreater(a, b)
         self.assertLess(b, a)
         self.assertEqual(a, a)
-        a = Ordinal([(100, 1)])  # w^100
-        b = Ordinal([(99, 3000)])  # w^99 * 3000
+        a = Ordinal([(100, 1)])
+        b = Ordinal([(99, 3000)])
 
         self.assertGreater(a, b)
         self.assertLess(b, a)
-        a = Ordinal([(2, 1), (1, 1), (0, 1)])  # w^2 + w + 1
-        b = Ordinal([(2, 1), (0, 1)])  # w^2 + 1
+        a = Ordinal([(2, 1), (1, 1), (0, 1)])
+        b = Ordinal([(2, 1), (0, 1)])
         self.assertGreater(a, b)
         self.assertLess(b, a)
 
-    def test_Manual(self):
-        a = Ordinal([(1, 1), (0, 1)])  # w + 1
-        b = Ordinal([(1, 1)])  # w
-        c = a + b  # w + 1 + w = w * 2
-        d = b + a  # w + w + 1 = w * 2 + 1
+    def test_manual(self):
+        a = Ordinal([(1, 1), (0, 1)])
+        b = Ordinal([(1, 1)])
+        c = a + b
+        d = b + a
         self.assertEqual(c, Ordinal([(1, 2)]))
         self.assertEqual(d, Ordinal([(1, 2), (0, 1)]))
-        a = Ordinal([(3, 1), (1, 1), (0, 1)])  # w^3 + w + 1
-        b = Ordinal([(2, 1)])  # w^2
-        c = a + b  # w^3 + w + 1 + w^2 = w^3 + w^2
+        a = Ordinal([(3, 1), (1, 1), (0, 1)])
+        b = Ordinal([(2, 1)])
+        c = a + b
         self.assertEqual(c, Ordinal([(3, 1), (2, 1)]))
-        a = Ordinal([(0, 0)])  # 0
-        b = Ordinal([(3, 1), (2, 2), (1, 3)])  # w^3 + w^2 * 2 + w * 3
+        a = Ordinal([(0, 0)])
+        b = Ordinal([(3, 1), (2, 2), (1, 3)])
         c = a + b
         d = b + a
         self.assertEqual(c, b)
         self.assertEqual(d, b)
         q = [(i, i) for i in reversed(range(1, 99))]
-        a = Ordinal(q)  # w^99 * 99 + w^98 * 98 + ... + w
-        b = Ordinal([(100, 1)])  # w^100
-        c = a + b  # w^99 * 99 + w^98 * 98 + ... + 0 + w^100 = w^100
+        a = Ordinal(q)
+        b = Ordinal([(100, 1)])
+        c = a + b
         self.assertEqual(c, b)
 
 
 class DijkstraOrdinalTest(TestCase):
 
     def test_manual(self):
-        a = Ordinal([(0, 1)])  # 1
-        b = Ordinal([(1, 1)])  # w
-        c = Ordinal([(1, 1), (0,1)])  # w + 1
-        d = Ordinal([(0, 0)])  # 0
+        a = Ordinal([(0, 1)])
+        b = Ordinal([(1, 1)])
+        c = Ordinal([(1, 1), (0, 1)])
+        d = Ordinal([(0, 0)])
         graph = [[(a, 1), (c, 2)], [(b, 2)], []]
         res = Dijkstra(graph, start=0, init_id=Ordinal([]))
         dist = res.getDistsList()
@@ -219,3 +221,56 @@ class DijkstraOrdinalTest(TestCase):
                     sm = max(sm, graph[dj.getShortestPath(j)[k-1]][dj.getShortestPath(j)[k]][0])
                 self.assertEqual(dist[i][j], sm)
 
+from libdist import Transport
+
+
+def rnd_edge(r):
+    u = r.randint(0, 1000)
+    return u, u + r.randint(0, 100)
+
+
+def filter_lst_inf(v):
+    return [None if c == 10**10 else c for c in v]
+
+
+class TransportTest(TestCase):
+    def test_inf_first(self):
+        problem = Transport(4)
+        problem.addEdge(0, 1, 9, 10)
+        problem.addEdge(1, 2, 11, 12)
+        problem.addEdge(1, 2, 8, 9)
+        problem.addEdge(0, 2, 20, 30)
+        problem.addEdge(0, 3, 7, 30)
+        problem.calc(0, start_time=9)
+        times = problem.getTimes()
+        self.assertEqual(times, {0: 9, 1: 10, 2: 12})
+        path = problem.getPath(2)
+        self.assertEqual(path, [0, 1, 2])
+
+    def test_inf_second(self):
+        problem = Transport(6)
+        problem.addEdge(1, 2, 5, 10)
+        problem.addEdge(2, 4, 10, 15)
+        problem.addEdge(5, 4, 0, 17)
+        problem.addEdge(4, 3, 17, 20)
+        problem.addEdge(3, 2, 20, 35)
+        problem.addEdge(1, 3, 2, 40)
+        problem.addEdge(3, 4, 40, 45)
+        problem.calc(1, start_time=0)
+        times = problem.getTimes()
+        self.assertEqual(times, {1: 0, 2: 10, 3: 20, 4: 15})
+        path = problem.getPath(3)
+        self.assertEqual(path, [1, 2, 4, 3])
+
+    def test_inf_third(self):
+        problem = Transport(6)
+        problem.addEdge(1, 3, 1, 2)
+        problem.addEdge(3, 4, 2, 10)
+        problem.addEdge(4, 5, 10, 20)
+        problem.addEdge(5, 4, 10, 15)
+        problem.addEdge(4, 2, 15, 40)
+        problem.calc(1, start_time=1)
+        times = problem.getTimes()
+        self.assertEqual(times, {1: 1, 2: 40, 3: 2, 4: 10, 5: 20})
+        path = problem.getPath(2)
+        self.assertEqual(path, [1, 3, 4, 2])
