1c3059ab4d9de673c892a4e88bb62f6a7a07ebc9
Daria Kolodzey <kolodaria@google.com>
2015-02-13 12:10:59 +0300

    created func for division into lower and upper
    Former-commit-id: bc7c0fbfafec16e6d24690fd2f0e41e44cac8960

--- Kolodzey/sem4/0-andrew/main.cpp
+++ Kolodzey/sem4/0-andrew/main.cpp
@@ -1,15 +1,51 @@
 #include <iostream>
 #include <cmath>
+#include <vector>
 
 using namespace std;
 
 struct Point {
+	Point() : x(0), y(0) { }
+	Point (long long x, long long y): x(x), y(y) {};
 	long long x;
 	long long y;
+	Point operator - (const Point& b) const {
+		return Point(x - b.x, y - b.y);
+	}
+	long long operator % (const Point& b) const {
+		return x * b.y - y * b.x;
+	}
 };
 
-const int MAX_N = 1005;
-Point vertices[MAX_N];
+bool compareX(Point a, Point b) {
+	return a.x < b.x;
+}
+
+//input --- vector of verteces sorted by x
+//output --- vector of vertices no lower than [vertices.front(), vertices.back()]
+vector<Point> chooseUpper(const vector<Point>& vertices){
+	vector<Point> ans;
+	ans.push_back(vertices.front());
+	for (auto i = vertices.begin() + 1; i != vertices.end() - 1; ++i) {
+		if (((vertices.back() - vertices.front()) % (*i - vertices.front())) >= 0)
+			ans.push_back(*i);
+	}
+	ans.push_back(vertices.back());
+	return ans;
+}
+
+//input --- vector of verteces sorted by x
+//output --- vector of vertices lower than [vertices.front(), vertices.back()]
+vector<Point> chooseLower(const vector<Point>& vertices){
+	vector<Point> ans;
+	ans.push_back(vertices.front());
+	for (auto i = vertices.begin() + 1; i != vertices.end() - 1; ++i) {
+		if (((vertices.back() - vertices.front()) % (*i - vertices.front())) < 0)
+			ans.push_back(*i);
+	}
+	ans.push_back(vertices.back());
+	return ans;
+}
 
 int main() {
 	
@@ -19,10 +55,21 @@ int main() {
 	double L; //distance from the castle
 	cin >> L;
 
+	vector<Point> vertices(N);
+
 	for (int i = 0; i < N; ++i)
 		cin >> vertices[i].x >> vertices[i].y;
 
-	
+	sort(vertices.begin(), vertices.end(), compareX);
 
+	vector<Point> upper = chooseUpper(vertices);
+	vector<Point> lower = chooseLower(vertices);
+
+	cout << "upper" << endl;
+	for (auto i = upper.begin(); i!=upper.end(); ++i)
+		cout << i->x << " " << i->y << endl;
+	cout << "lower" << endl;
+	for (auto i = lower.begin(); i!=lower.end(); ++i)
+		cout << i->x << " " << i->y << endl;
 	return 0;
 }
