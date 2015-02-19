a0d063ef2be9adea3cdca9ac60e8210531402d00
Daria Kolodzey <kolodaria@google.com>
2015-02-19 21:14:31 +0300

    timus-working version
    Former-commit-id: 49b5c8feaa17536cef0bceeee4c9c73a4364b0c6

--- Kolodzey/sem4/0-andrew/main.cpp
+++ Kolodzey/sem4/0-andrew/main.cpp
@@ -1,99 +1,110 @@
+//Dashk0 submitted this solution to Timus and successeded
+
 #include <iostream>
-#include <cmath>
 #include <vector>
+#include <algorithm>
+#include <cmath>
+
+const double PI = 3.1415965;
 
 using namespace std;
 
 struct Point {
-	Point() : x(0), y(0) { }
-	Point (long long x, long long y): x(x), y(y) {};
-	long long x;
-	long long y;
-	Point operator - (const Point& b) const {
-		return Point(x - b.x, y - b.y);
-	}
-	long long operator % (const Point& b) const {
-		return x * b.y - y * b.x;
-	}
-	double len() {
-		return sqrt(x * x + y * y);
-	}
+    Point() : x(0), y(0) { }
+    Point (long long x, long long y): x(x), y(y) {};
+    long long x;
+    long long y;
+    Point operator - (const Point& b) const {
+        return Point(x - b.x, y - b.y);
+    }
+    long long operator % (const Point& b) const {
+        return x * b.y - y * b.x;
+    }
+    double len() {
+        return sqrt(x * x + y * y);
+    }
 };
 
 bool compareX(Point a, Point b) {
-	return a.x < b.x;
+    return a.x < b.x;
 }
 
 //input --- vector of verteces sorted by x
 //output --- vector of vertices no lower than [vertices.front(), vertices.back()]
 vector<Point> chooseUpper(const vector<Point>& vertices){
-	vector<Point> ans;
-	ans.push_back(vertices.front());
-	for (auto i = vertices.begin() + 1; i != vertices.end() - 1; ++i) {
-		if (((vertices.back() - vertices.front()) % (*i - vertices.front())) >= 0)
-			ans.push_back(*i);
-	}
-	ans.push_back(vertices.back());
-	return ans;
+    vector<Point> ans;
+    ans.push_back(vertices.front());
+    for (auto i = vertices.begin() + 1; i != vertices.end() - 1; ++i) {
+        if (((vertices.back() - vertices.front()) % (*i - vertices.front())) >= 0)
+            ans.push_back(*i);
+    }
+    ans.push_back(vertices.back());
+    return ans;
 }
 
 //input --- vector of verteces sorted by x
 //output --- vector of vertices lower than [vertices.front(), vertices.back()]
 vector<Point> chooseLower(const vector<Point>& vertices){
-	vector<Point> ans;
-	ans.push_back(vertices.front());
-	for (auto i = vertices.begin() + 1; i != vertices.end() - 1; ++i) {
-		if (((vertices.back() - vertices.front()) % (*i - vertices.front())) < 0)
-			ans.push_back(*i);
-	}
-	ans.push_back(vertices.back());
-	return ans;
+    vector<Point> ans;
+    ans.push_back(vertices.front());
+    for (auto i = vertices.begin() + 1; i != vertices.end() - 1; ++i) {
+        if (((vertices.back() - vertices.front()) % (*i - vertices.front())) < 0)
+            ans.push_back(*i);
+    }
+    ans.push_back(vertices.back());
+    return ans;
 }
 
-double countLenghtOfConvexHullHalf(forward_iterator_tag begin, forward_iterator_tag end) {
-	vector<Point> stack;
-	for (auto i = begin; i != end; ++i) {
-		while((stack.size() > 1) &&
-			  (((stack.back() - stack[stack.size() - 2]) % (*i - stack.back())) >= 0))
-			stack.pop_back();
-		stack.push_back(*i);
-	}
-	double ans = 0;
-	for (auto i = 0; i < stack.size() - 1; ++i)
-		ans  += (stack[i + 1] - stack[i]).len();
-	return ans;
+template<typename T>
+
+double countLenghtOfConvexHullHalf(T begin, T end) {
+    vector<Point> stack;
+    for (auto i = begin; i != end; ++i) {
+        while((stack.size() > 1) &&
+              (((stack.back() - stack[stack.size() - 2]) % (*i - stack.back())) >= 0))
+            stack.pop_back();
+        stack.push_back(*i);
+    }
+    double ans = 0;
+    for (auto i = 0; i < stack.size() - 1; ++i) {
+        //cout << stack[i].x << " " << stack[i].y << endl;
+        ans  += (stack[i + 1] - stack[i]).len();
+    }
+    //cout << stack.back().x << " " << stack.back().y << endl;
+    return ans;
 }
 
 int main() {
-	
-	int N; //number of vertices
-	cin >> N;
-	
-	double L; //distance from the castle
-	cin >> L;
+    
+    int N; //number of vertices
+    cin >> N;
+    
+    double L; //distance from the castle
+    cin >> L;
 
-	vector<Point> vertices(N);
+    vector<Point> vertices(N);
 
-	for (int i = 0; i < N; ++i)
-		cin >> vertices[i].x >> vertices[i].y;
+    for (int i = 0; i < N; ++i)
+        cin >> vertices[i].x >> vertices[i].y;
 
-	sort(vertices.begin(), vertices.end(), compareX);
+    sort(vertices.begin(), vertices.end(), compareX);
 
-	vector<Point> upper = chooseUpper(vertices);
-	vector<Point> lower = chooseLower(vertices);
+    vector<Point> upper = chooseUpper(vertices);
+    vector<Point> lower = chooseLower(vertices);
 
-	/*
-	Testing lower and upper
-	cout << "upper" << endl;
-	for (auto i = upper.begin(); i!=upper.end(); ++i)
-		cout << i->x << " " << i->y << endl;
-	cout << "lower" << endl;
-	for (auto i = lower.begin(); i!=lower.end(); ++i)
-		cout << i->x << " " << i->y << endl;
-	*/
+    /*
+    Testing lower and upper
+    cout << "upper" << endl;
+    for (auto i = upper.begin(); i!=upper.end(); ++i)
+        cout << i->x << " " << i->y << endl;
+    cout << "lower" << endl;
+    for (auto i = lower.begin(); i!=lower.end(); ++i)
+        cout << i->x << " " << i->y << endl;
+    */
+    double len_of_convex_hull = countLenghtOfConvexHullHalf(upper.begin(), upper.end()) +
+          countLenghtOfConvexHullHalf(lower.rbegin(), lower.rend());
+          
+    cout << ((long long)(len_of_convex_hull + (L * 2 * PI + 0.5))) << endl;
 
-	cout << countLenghtOfConvexHullHalf(upper.begin(), upper.end()) << endl;
-	cout << countLenghtOfConvexHullHalf(lower.rbegin(), lower.rend()) << endl;
-
-	return 0;
+    return 0;
 }
